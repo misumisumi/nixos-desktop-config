69ceb8934f82da0747384bce6b4141bf9ad98fc5
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-09-29 14:05:57 +0900

    add ime.nix

---
+++ apps/desktop/ime.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  i18n = {
+    inputMethod = {
+      enabled = 'fcitx5';
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-gtk
+          fcitx5-qt
+          fcitx5-mozc
+          fcitx5-configtool
+        ];
+      };
+    };
+  };
+}
