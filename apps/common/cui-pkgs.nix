f69b3a2933422b6c0576c96bd3d022a660c44c99
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-09-29 13:47:15 +0900

    add cui-pkg.nix

---
+++ apps/common/cui-pkgs.nix
@@ -0,0 +1,13 @@
+[
+  bottom    # System monitor
+  duf       # Show storage usage
+  neofetch  # Fetch system info
+
+  ranger    # Filer
+  tree      # Show file tree
+
+  ffmpeg    # Multi media solution
+
+  figlet    # Make AA from character
+  cmatrix   # Lain of character
+];
