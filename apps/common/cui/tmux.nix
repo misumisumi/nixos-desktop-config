52ff2ad13143bd6cfe8aa934e465d49c903926be
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-09-29 13:27:03 +0900

    Done move .tmux.conf to tmux.nix

--- apps/common/cui/tmux.nix
+++ apps/common/cui/tmux.nix
@@ -26,9 +26,46 @@
 
       prefix = "C-j";
 
-      terminal = "tmux-256color";
+      terminal = "screen-256color";
       clock24 = true;
+      historyLimit = 10000;
+      customPaneNavigationAndResize = true;
+      keyMode = "vi";
+      resizeAmount = 5;
+      shell = "${pkgs.zsh}/bin/zsh";
 
+      extraConfig = ''
+        set -g mouse on
+
+        # Emulate visual-mode in copy-mode of tmux & copy buffer to xsel
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xsel -bi"
+        bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "xsel -bi"
+
+        bind-key b break-pane
+        bind-key q kill-pane
+        bind-key C-q kill-session
+        #bind-k display-panes\; kill-pane\;
+        #bind-k display-panes\; confirm-before kill-pane\;
+        bind-key C-x run "tmux kill-pane || tmux kill-window"
+        bind-key C-t run "tmux last-pane || tmux last-window || tmux new-window"
+
+        bind-key i display-panes
+
+        # relox config file
+        bind-key r source-file ~/.tmux.conf \; display "Reloaded!"
+
+        unbind s
+        bind-key s split-window -v # 水平方向split
+        bind-key v split-window -h # 垂直方向split
+
+        run-shell "tmux setenv -g TMUX_VERSION $(tmux -V | cut -c 6-)"
+
+        if-shell -b '[ "$(echo "$TMUX_VERSION < 3.0" | bc)" = 1 ]' "bind-key S choose-tree -s"
+        if-shell -b '[ "$(echo "$TMUX_VERSION >= 3.0" | bc)" = 1 ]' "bind-key S choose-tree -Zs"
+      '';
     };
+
   };
 }
