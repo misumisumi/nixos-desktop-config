cff9651a8c6b1b2cf2d110c6fb6ce5fad2d4eade
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-01 18:17:58 +0900

    ホームマネージャーで管理されないGUI pkgsを追加

---
+++ apps/desktop/extra-pkgs.nix
@@ -0,0 +1,49 @@
+[
+  nomacs
+
+  thunar
+  thunar-volman
+  thunar-archive-plugin
+  thunar-media-tags-plugin
+
+  discord
+  slack
+
+  audacity
+  sox
+  gnuplot
+
+  blender
+  krita
+  gmic-qt-krita
+  gpick
+  inkspace
+  unityhub
+
+  android-tools
+  sidequest
+
+  gnome.simple-scan
+
+  mosh
+
+  nextcloud-client
+
+  mesa-demos
+  vulkan-tools
+
+  pandoc
+
+  spotify
+  spotify-tui
+
+  zathura-with-plugins
+
+  baobab
+
+  CopyQ
+  conky
+
+  polkit-gnome
+]
+
