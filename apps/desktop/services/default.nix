3c487c130fd7120efbbf6425872e1bc1dd358bea
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-01 17:00:21 +0900

    add services

--- apps/desktop/services/default.nix
+++ apps/desktop/services/default.nix
@@ -1,16 +1,104 @@
-[
-  blueman
-  bluez
-  bluez-tools
+{ pkgs, ... }:
 
-  baobab
+{
+  i18n = {
+    inputMethod = {
+      enabled = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-gtk
+          fcitx5-qt
+          fcitx5-mozc
+          fcitx5-configtool
+        ];
+      };
+    };
+  };
 
-  copyq
-  conky
+  services = {
+    picom = {
+      enable = true;
+      backend = "glx";
+      experimentalBackends = true;
+      vSync = false;
 
-  flameshot
+      fade = true;
+      fadeDelta = 10;
+      fadeSteps = [ 0.06 0.06 ];
 
-  kdeconnect-kde
+      activeOpacity = 0.75;
+      inactiveOpacity = 0.75;
+      opacityRules = [
+        "85:class_g = 'kitty' && focused"
+        "85:class_g = 'kitty' && !focused"
+        "100:class_g = 'krita' && focused"
+        "100:class_g = 'krita' && !focused"
+        "100:class_g = 'Blender' && focused"
+        "100:class_g = 'Blender' && !focused"
+        "100:class_g = 'Unity' && focused"
+        "100:class_g = 'Unity' && !focused"
+        "100:class_g = 'fcitx' && focused"
+        "100:class_g = 'fcitx' && !focused"
+        "100:class_g = 'pentablet' && focused"
+        "100:class_g = 'pentablet' && !focused"
+        "100:name = 'masterduel' && focused"
+        "100:name = 'masterduel' && !focused"
+        "100:name = 'i3lock' && focused"
+        "100:name = 'i3lock' && !focused"
+      ];
+       
+      shadow = true;
+      shadowExclude = [
+        "name = 'Notification'"
+        "class_g = 'Conky'"
+        "class_g ?= 'Notify-osd'"
+        "class_g = 'Cairo-clock'"
+        "_GTK_FRAME_EXTENTS@:c"
+        "class_g = 'rofi'"
+      ];
+      shadowOffsets = [-7 -7];
+      shadowOpacity = 0.75;
 
-  polkit-gnome
-]
+      settings = {
+        shadow-radius = 16;
+        frame-opacity = 0.8;
+        blur = {
+          method = "dual_kawase";
+          size = 8;
+          strength = 5;
+          background = true;
+          background-frame = true;
+          bluer-kern = "3x3box";
+          background-exclude = [
+            "window_type = 'dock'"
+            "window_type = 'desktop'"
+            "_GTK_FRAME_EXTENTS@:c"
+          ];
+        mark-wmwin-focused = true;
+        mark-overdir-focused = false;
+        detect-rounded-corners = true;
+        detect-client-opacity = true;
+        detect-transient = true;
+        glx-no-stencil = true;
+        glx-no-rebind-pixmap = true;
+        use-damage = true;
+        xrender-sync-fence = true;
+        };
+      };
+    };
+
+    blueman-applet = {
+      enable = true;
+    };
+
+    flameshot = {
+      enable = true;
+    };
+
+    kdeconnect = {
+      enable = true;
+      indicator = true;
+    };
+  };
+}
+
