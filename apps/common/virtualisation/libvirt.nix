4c8841af225ea3e58a75f65d4e2156731d33ceb0
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-09 15:27:08 +0900

    add virtualisation settings

---
+++ apps/common/virtualisation/libvirt.nix
@@ -0,0 +1,46 @@
+{ pkgs, user, ... }:
+
+{
+  boot = {
+  extraModprobeConfig = ''
+    options kvm_amd nested=1
+    options kvm ignore_nsrs=1
+  '';
+  # kernelParams = [ "amd_iommu=on" "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ]; 
+  # kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" "vfio_virqfd" ];
+  # extraModprobeConfig = "options vfio-pci ids=1002:67DF,1002:AAF0";
+  };
+
+  users = {
+    groups = {
+      libvirtd = {
+        members = [ "root", "${user}" ];
+      };
+    };
+  };
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;                          # Virtual drivers
+      qemu = {
+        verbatimConfig = ''
+          nvram = [ 
+            "${pkgs.OVMFFull}/FV/OVMF.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
+            "${pkgs.OVMFFull}/FV/OVMF.secboot.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
+          ]
+        '';
+      };
+    };
+    spiceUSBRedirection.enable = true;        # USB passthrough
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      qemu
+      OVMFFull
+      virt-manager
+      looking-glass-client
+    ];
+  };
+
+}
---
+++ apps/common/virtualisation/podman.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  virtualisation = {
+    podman = {
+      enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.dnsname.enable = true;
+    };
+
+    oci-containers = {
+      backend = "podman";
+    };
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      buildah
+    ];
+  };
+}
