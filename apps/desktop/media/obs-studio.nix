6bee5a654c7959cc570f76da0ca44a61549aa26f
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-10 22:30:32 +0900

    fix plugin pkgs for obs

--- apps/desktop/media/obs-studio.nix
+++ apps/desktop/media/obs-studio.nix
@@ -4,16 +4,16 @@
   programs = {
     obs-studio = {
       enable = true;
-      plugins = [
-        "obs-studio-plugins.obs-ndi"
-        "obs-studio-plugins.looking-glass-obs"
-        "obs-studio-plugins.obs-pipewire-audio-capture"
-        pkgs.fetchFromGitHub {
-          owner = "dev47apps";
-          repo = "droidcam-obs-plugin";
-          rev = "1.6.0";
-          sha256 = "083pv2l8d1hnfz00y0dfaid87qxwzn23077ciz5z4rn118rxkfr0";
-        }
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-ndi
+        looking-glass-obs
+        obs-pipewire-audio-capture
+        # pkgs.fetchFromGitHub {
+        #   owner = "dev47apps";
+        #   repo = "droidcam-obs-plugin";
+        #   rev = "1.6.0";
+        #   sha256 = "083pv2l8d1hnfz00y0dfaid87qxwzn23077ciz5z4rn118rxkfr0";
+        # }
       ];
     };
   };
