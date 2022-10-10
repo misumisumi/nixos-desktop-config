26ccc9c01ccafe459b60456860ed684882e6f199
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-10 21:21:23 +0900

    add loading personal config for powerlevel10k

--- apps/common/shell/zsh.nix
+++ apps/common/shell/zsh.nix
@@ -74,6 +74,7 @@
         if [[ -r "''${XDG_CACHE_HOME:-''${HOME}/.cache}"/p10k-instant-prompt-"''${(%):-%n}".zsh ]]; then
           source "''${XDG_CACHE_HOME:-''${HOME}/.cache}"/p10k-instant-prompt-"''${(%):-%n}".zsh
         fi
+        source "''${XDG_CACHE_HOME}/zsh/.p10k.zsh"
 
         ZVM_VI_INSERT_ESCAPE_BINDKEY=jj
         ZVM_VI_VISUAL_ESCAPE_BINDKEY=jj
