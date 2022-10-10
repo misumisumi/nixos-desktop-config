faac6e6d8794256597354efcdccf5c5af8e346c4
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-10 22:31:36 +0900

    obsのプラグインはgithubから取れる?

--- apps/desktop/media/obs-studio.nix
+++ apps/desktop/media/obs-studio.nix
@@ -4,16 +4,17 @@
   programs = {
     obs-studio = {
       enable = true;
-      plugins = with pkgs.obs-studio-plugins; [
+      plugins = [
+        pkgs.fetchFromGitHub {
+          owner = "dev47apps";
+          repo = "droidcam-obs-plugin";
+          rev = "1.6.0";
+          sha256 = "083pv2l8d1hnfz00y0dfaid87qxwzn23077ciz5z4rn118rxkfr0";
+        }
+      ] ++ with pkgs.obs-studio-plugins; [
         obs-ndi
         looking-glass-obs
         obs-pipewire-audio-capture
-        # pkgs.fetchFromGitHub {
-        #   owner = "dev47apps";
-        #   repo = "droidcam-obs-plugin";
-        #   rev = "1.6.0";
-        #   sha256 = "083pv2l8d1hnfz00y0dfaid87qxwzn23077ciz5z4rn118rxkfr0";
-        # }
       ];
     };
   };
