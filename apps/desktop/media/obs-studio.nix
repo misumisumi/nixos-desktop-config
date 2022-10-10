0b88ccc721dc15028375a5d4904bb5b600e14b06
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-10 22:34:32 +0900

    自作のobsのプラグインの作製方法は調べないといけない

--- apps/desktop/media/obs-studio.nix
+++ apps/desktop/media/obs-studio.nix
@@ -8,12 +8,12 @@
         obs-studio-plugins.obs-ndi
         obs-studio-plugins.looking-glass-obs
         obs-studio-plugins.obs-pipewire-audio-capture
-        fetchFromGitHub {
-          owner = "dev47apps";
-          repo = "droidcam-obs-plugin";
-          rev = "1.6.0";
-          sha256 = "083pv2l8d1hnfz00y0dfaid87qxwzn23077ciz5z4rn118rxkfr0";
-        }
+        # fetchFromGitHub {
+        #   owner = "dev47apps";
+        #   repo = "droidcam-obs-plugin";
+        #   rev = "1.6.0";
+        #   sha256 = "083pv2l8d1hnfz00y0dfaid87qxwzn23077ciz5z4rn118rxkfr0";
+        # }
       ];
     };
   };
