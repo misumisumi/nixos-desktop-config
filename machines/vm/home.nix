76f50a854b737641c6ab1bc39318f2fee4fe6c8f
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-10 13:21:53 +0900

    add stateVersion

--- machines/vm/home.nix
+++ machines/vm/home.nix
@@ -2,7 +2,7 @@
 # Home-manager configuration for mother
 #
 # flake.nix
-{ pkgs, ... }:
+{ pkgs, stateVersion, ... }:
 
 {
   imports = (import ../../apps/common/cui) ++
@@ -10,6 +10,7 @@
             (import ../../apps/common/shell);
 
   home = {
+    stateVersion = stateVersion;
     packages = with pkgs; (import ../../apps/common/extra-pkgs.nix);
   };
 }
