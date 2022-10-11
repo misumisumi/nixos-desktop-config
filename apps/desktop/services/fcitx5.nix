fbcdbb7537f594c676c93ef37baef6403c717f3f
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-11 15:50:49 +0900

    fix pkg name

--- apps/desktop/services/fcitx5.nix
+++ apps/desktop/services/fcitx5.nix
@@ -5,12 +5,12 @@
     inputMethod = {
       enabled = "fcitx5";
       fcitx5 = {
-        addons = [
-          pkgs.fcitx5-gtk
-          pkgs.libsForQt5.fcitx5-qt
-          pkgs.fcitx5-mozc
-          pkgs.fcitx5-configtool
-          pkgs.nur.repos.xddxdd.fcitx5-breeze
+        addons = with pkgs; [
+          fcitx5-gtk
+          libsForQt5.fcitx5-qt
+          fcitx5-mozc
+          fcitx5-configtool
+          nur.repos.xddxdd.fcitx5-breeze
         ];
       };
     };
