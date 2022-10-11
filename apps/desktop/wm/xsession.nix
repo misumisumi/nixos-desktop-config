4693a62a8069453be88d38bc510875230a8b4d83
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-11 20:03:26 +0900

    fix wallpapers path

--- apps/desktop/wm/xsession.nix
+++ apps/desktop/wm/xsession.nix
@@ -2,7 +2,7 @@
 
 {
   home.packages = with pkgs; [ qtile ];
-  home.file."${config.home.homeDirectory}/Pictures".source = ./qtile/wallpapers;
+  home.file."${config.home.homeDirectory}/Pictures/wallpapers".source = ./qtile/wallpapers;
   xdg = {
     configFile = {
       "qtile".source = ./qtile;    # windowsとconfigを共有するため.config/nvimで管理する
