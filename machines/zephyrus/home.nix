1e4e125efb5a933e07521ed296e27795251aceaf
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-12 21:21:16 +0900

    add conf for zephyrus

---
+++ machines/zephyrus/home.nix
@@ -0,0 +1,22 @@
+#
+# Home-manager configuration for mother
+#
+# flake.nix
+{ config, pkgs, ... }:
+
+{
+  imports = (import ../../apps/common/cui) ++
+            (import ../../apps/common/neovim) ++
+            (import ../../apps/common/shell) ++
+            (import ../../apps/desktop);
+
+  home = {
+    packages = (import ../../apps/common/pkgs) pkgs ++
+               (import ../../apps/desktop/pkgs) pkgs;
+  };
+
+  xresources = {
+    extraConfig = "Xft.dpi:100";
+  };
+
+}
