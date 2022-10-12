7344137f1811fab0d150606d30a860bf22d79b29
Sumiharu Kobayashi <dragon511southern@gmail.com>
2022-10-12 14:28:34 +0900

    commit enable nvidia gpu using podman

--- apps/common/virtualisation/podman.nix
+++ apps/common/virtualisation/podman.nix
@@ -4,6 +4,7 @@
   virtualisation = {
     podman = {
       enable = true;
+      enableNvidia = true;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
       # Required for containers under podman-compose to be able to talk to each other.
