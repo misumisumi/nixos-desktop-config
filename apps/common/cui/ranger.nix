500d299c1dfd8138e30d12a439e0fe147ce9fba0
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-15 03:11:07 +0900

    link ranger conf to .config

--- apps/common/cui/ranger.nix
+++ apps/common/cui/ranger.nix
@@ -4,9 +4,7 @@
   home.packages = with pkgs; [ ranger ueberzug ];
   xdg = {
     configFile = {
-      "ranger/rc.conf".source = ./ranger/rc.conf;
-      "ranger/rifle.conf".source = ./ranger/rifle.conf;
-      "ranger/scope.sh".source = ./ranger/scope.sh;
+      "ranger".source = ./ranger;
     };
   };
 }
