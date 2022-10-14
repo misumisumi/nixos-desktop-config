084d79043c573b5e312e3cb1c0df0048bc17c0ed
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-15 01:54:19 +0900

    add gpu.nix

--- machines/extra/default.nix
+++ machines/extra/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ./network.nix
+    ./gpu.nix
     ../common/pulseaudio.nix
     ../common/printer.nix
     ../../apps/desktop/wm/configuration.nix
