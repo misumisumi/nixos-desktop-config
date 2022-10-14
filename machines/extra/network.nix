e34cde0802e7c6ea68dc8fa3af16502424065733
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-14 22:30:05 +0900

    fix useDHCP

--- machines/extra/network.nix
+++ machines/extra/network.nix
@@ -5,7 +5,6 @@
     wireless = {
       enable = true;
     };
-    useDHCP = lib.mkDefault true;
     dhcpcd = {
       enable = true;
       wait = "background";
@@ -37,28 +36,3 @@
     };
   };
 }
-# WiFi setting
-# networking = {
-#   wireless = {
-#     networks = {
-#       "<name>" = {
-#         psk = ""; # psk hash if null psk is NONE
-#         priority = "";
-#       };
-#     };
-#   };
-# };
-
-#  networking.interfaces.enp8s0.useDHCP = true;
-#  networking.interfaces.br0.useDHCP = false;
-#  networking.bridges = {
-#    "br0" = {
-#      interfaces = [ "enp8s0" ];
-#    };
-#  };
-# networking.interfaces.br0.ipv4.addresses = [ {
-#   address = "10.10.10.11";
-#   prefixLength = 24;
-# } ];
-# networking.defaultGateway = "10.10.10.1";
-# networking.nameservers = ["10.10.10.1" "8.8.8.8"];
