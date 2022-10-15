26005fbf7a10d991e70d76ac5fc2e83b500b630f
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-15 13:38:26 +0900

    widgetをまとめて管理できるようにした

--- apps/desktop/wm/qtile/my_modules/widgets.py
+++ apps/desktop/wm/qtile/my_modules/widgets.py
@@ -22,6 +22,33 @@ _colorset6 = {'background': PARAM.c_normal['BGbase'], 'foreground': PARAM.c_norm
 _colorset7 = {'background': PARAM.c_normal['clear'], 'foreground': PARAM.c_normal['cyan']}
 _colorset8 = {'background': PARAM.c_normal['BGbase'], 'foreground': PARAM.c_normal['magenta']}
 
+groupbox = widget.GroupBox(this_current_screen_border=PARAM.c_normal['cyan'], borderwidth=PARAM.border, **_colorset3,
+                           font='Hack Nerd Font', fontsize=PARAM.font_size+2,
+                           active=PARAM.c_normal['white']),
+cpu = widget.CPU(format=' {load_percent:0=4.1f}%', **_colorset2),
+memory = widget.Memory(format=' {MemUsed:0=4.1f}{mm}/{MemTotal: .1f}{mm}',
+                       measure_mem='G', measure_swap='G', **_colorset1),
+df = widget.DF(format = " {uf}{m}/{s}{m} ({r:.0f}%)", visible_on_warn=False,
+               partition='/home', **_colorset2),
+chrod = widget.Chord(**_colorset8),
+wttr = widget.Wttr(format='%c%t/%p|', location={'Himeji':'Himeji'}, **_colorset2),
+clock = widget.Clock(format='%y-%m-%d %a %H:%M:%S', **_colorset2),
+tasklist = widget.TaskList(border=PARAM.c_normal['BGbase'], theme_mode="prefeered", theme_path="/run/current-system/sw/share/icons/Papirus-Dark", icon_size=PARAM.font_size, borderwidth=PARAM.border, max_title_width=120, **_colorset3),
+net = widget.Net(format='{down} ↓↑ {up}', **_colorset2),
+volume = widget.Volume(fmt=' {}',   
+                       get_volume_command = ["sh", "-c", "if [ -z \"$(pactl get-sink-mute $(pactl get-default-sink) | sed -e 's/Mute: no//g')\" ]; then echo \[$(pactl get-sink-volume $(pactl get-default-sink) | awk -F'/' '{print $2}' | sed -e 's/\s//g')\]; else echo M; fi"],
+                       mute_command = ["pactl set-source-mute @DEFAULT_SOURCE@ toggle"],
+                       volume_up_command = ["pactl set-sink-volume @DEFAULT_SINK@ +5%"],
+                       volume_down_command = ["pactl set-sink-volume @DEFAULT_SINK@ -5%"],
+                       **_colorset1),
+current_screen = widget.CurrentScreen(active_color=PARAM.c_normal['magenta'],
+                                      inactive_color=PARAM.c_normal['BGbase'],
+                                      inactive_text='N', **_colorset2),
+systray = widget.Systray(**_colorset3),
+backlight = list(Path('/sys/class/backlight/').glob('*'))
+backlight = widget.Backlight(fmt=' {}', backlight_name=backlight[0], **_colorset2),
+battery = widget.Battery(format='{char} {percent:2.0%}', charge_char='', discharge_char='',
+                         empty_char='', full_chal='', unknown_char='', **_colorset1),
 
 def _left_corner(background, foreground):
     return widget.TextBox(
@@ -49,42 +76,37 @@ def _separator():
         padding = 2
     )
 
-
 def make_widgets(is_tray=False):
     top_widgets = [
         _left_corner(**_colorset4),
-        widget.GroupBox(this_current_screen_border=PARAM.c_normal['cyan'], borderwidth=PARAM.border, **_colorset3,
-                        font='Hack Nerd Font', fontsize=PARAM.font_size+2,
-                        active=PARAM.c_normal['white']),
+        groupbox
         _rignt_corner(**_colorset4),
         ]
     if not (PARAM.laptop or PARAM.vm):
         top_widgets += [
             _separator(),
             _left_corner(**_colorset1),
-            widget.CPU(format=' {load_percent:0=4.1f}%', **_colorset2),
+            cpu
             _rignt_corner(**_colorset1),
-            widget.Memory(format=' {MemUsed:0=4.1f}{mm}/{MemTotal: .1f}{mm}',
-                          measure_mem='G', measure_swap='G', **_colorset1),
+            memory
             _rignt_corner(**_colorset2),
-            widget.DF(format = " {uf}{m}/{s}{m} ({r:.0f}%)", visible_on_warn=False,
-                      partition='/home', **_colorset2),
+            df
             _rignt_corner(**_colorset1),
         ]
     top_widgets += [
         _separator(),
-        widget.Chord(**_colorset8),
+        chrod
         widget.Spacer(),
         _left_corner(**_colorset1),
-        widget.Wttr(format='%c%t/%p|', location={'Himeji':'Himeji'}, **_colorset2),
-        widget.Clock(format='%y-%m-%d %a %H:%M:%S', **_colorset2),
+        wttr
+        clock
         _rignt_corner(**_colorset1),
         ]
 
     top_widgets += [
         _separator(),
         _left_corner(**_colorset4),
-        widget.TaskList(border=PARAM.c_normal['BGbase'], icon_size=PARAM.font_size, borderwidth=PARAM.border, max_title_width=120, **_colorset3),
+        tasklist
         _rignt_corner(**_colorset4),
         _separator()
     ]
@@ -92,70 +114,49 @@ def make_widgets(is_tray=False):
     if not (PARAM.laptop or PARAM.vm):
         top_widgets += [
             _left_corner(**_colorset1),
-            widget.Net(format='{down} ↓↑ {up}', **_colorset2),
+            net
             _rignt_corner(**_colorset1),
-            # widget.PulseVolume(fmt=' {}', limit_max_volume=True, volume_app='pavucontrol',
-            #                    update_interval=0.1, **_colorset1),
-            widget.Volume(fmt=' {}',   
-                          get_volume_command = ["sh", "-c", "if [ -z \"$(pactl get-sink-mute $(pactl get-default-sink) | sed -e 's/Mute: no//g')\" ]; then echo \[$(pactl get-sink-volume $(pactl get-default-sink) | awk -F'/' '{print $2}' | sed -e 's/\s//g')\]; else echo M; fi"],
-                          mute_command = ["pactl set-source-mute @DEFAULT_SOURCE@ toggle"],
-                          volume_up_command = ["pactl set-sink-volume @DEFAULT_SINK@ +5%"],
-                          volume_down_command = ["pactl set-sink-volume @DEFAULT_SINK@ -5%"],
-                          **_colorset1),
+            volume
             _rignt_corner(**_colorset2),
-            widget.CurrentScreen(active_color=PARAM.c_normal['magenta'],
-                                 inactive_color=PARAM.c_normal['BGbase'],
-                                 inactive_text='N', **_colorset2),
+            current_screen
             _rignt_corner(**_colorset1)
             ]
     if is_tray:
         top_widgets += [
             _separator(),
             _left_corner(**_colorset4),
-            widget.Systray(**_colorset3),
-            # widget.StatusNotifier(**_colorset3),
+            systray
             _rignt_corner(**_colorset4),
             _separator(),
         ]
 
     if PARAM.laptop or PARAM.vm:
-        backlight = list(Path('/sys/class/backlight/').glob('*'))
         bottom_widgets = [
             _separator(),
             _left_corner(**_colorset7),
-            widget.CPU(format=' {load_percent:0=4.1f}%', **_colorset2),
+            cpu
             _rignt_corner(**_colorset1),
-            widget.Memory(format=' {MemUsed:0=4.1f}{mm}/{MemTotal: .1f}{mm}',
-                          measure_mem='G', measure_swap='G', **_colorset1),
+            memory
             _rignt_corner(**_colorset2),
-            widget.DF(format = " {uf}{m}/{s}{m} ({r:.0f}%)", visible_on_warn=False,
-                      partition='/home', **_colorset2),
+            df
             _rignt_corner(**_colorset7),
             widget.Spacer(),
 
             _left_corner(**_colorset7),
-            widget.Net(format='{down} ↓↑ {up}', **_colorset2),
+            net
             _rignt_corner(**_colorset1),
-            widget.Volume(fmt=' {}',   
-                          get_volume_command = ["sh", "-c", "if [ -z \"$(pactl get-sink-mute $(pactl get-default-sink) | sed -e 's/Mute: no//g')\" ]; then echo \[$(pactl get-sink-volume $(pactl get-default-sink) | awk -F'/' '{print $2}' | sed -e 's/\s//g')\]; else echo M; fi"],
-                          mute_command = ["pactl set-source-mute @DEFAULT_SOURCE@ toggle"],
-                          volume_up_command = ["pactl set-sink-volume @DEFAULT_SINK@ +5%"],
-                          volume_down_command = ["pactl set-sink-volume @DEFAULT_SINK@ -5%"],
-                          **_colorset1),
+            volume
             _rignt_corner(**_colorset2)
         ]
         if PARAM.laptop:
             bottom_widgets += [
-                widget.Backlight(fmt=' {}', backlight_name=backlight[0], **_colorset2),
+                backlight
                 _rignt_corner(**_colorset1),
-                widget.Battery(format='{char} {percent:2.0%}', charge_char='', discharge_char='',
-                               empty_char='', full_chal='', unknown_char='', **_colorset1),
+                battery
                 _rignt_corner(**_colorset2),
             ]
         bottom_widgets += [
-            widget.CurrentScreen(active_color=PARAM.c_normal['magenta'],
-                                 inactive_color=PARAM.c_normal['BGbase'],
-                                 inactive_text='N', **_colorset2),
+            current_screen
             _rignt_corner(**_colorset7)
         ]
     else:
