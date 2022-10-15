cadab1ef6e675af9a0c2b0ca2b10cb22eaf3c603
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-15 13:06:38 +0900

    fix kitty config

--- apps/desktop/terminal/kitty.nix
+++ apps/desktop/terminal/kitty.nix
@@ -4,6 +4,16 @@
   programs = {
     kitty = {
       enable = true;
+      font = {
+        package = (nerdfonts.override {                   # Nerdfont override
+                    fonts = [
+                      "FiraCode"
+                      "Hack"
+                    ];
+                  });
+        name = "FiraCode Nerd Font";
+        size = 14.0;
+      };
     };
   };
 }
