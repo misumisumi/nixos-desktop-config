1dbc9ecba3cd77d00818a36bb846c10eca335970
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-15 12:55:36 +0900

    fix ranger pkgs

--- apps/common/cui/ranger.nix
+++ apps/common/cui/ranger.nix
@@ -1,20 +1,19 @@
 { pkgs, ... }:
+let
+ranger = pkgs.ranger.overrideAttrs (r: {
+  preConfigure = r.preConfigure + ''
+    # Specify path to Überzug
+    substituteInPlace ranger/ext/img_display.py \
+      --replace "Popen(['ueberzug'" "Popen(['${pkgs.ueberzug}/bin/ueberzug'"
 
+    # Use Überzug as the default method
+    substituteInPlace ranger/config/rc.conf \
+      --replace 'set preview_images_method w3m' 'set preview_images_method ueberzug'
+    '';
+  });
+in
 {
-  home.packages = with pkgs; [
-    ueberzug 
-    ranger.overrideAttrs (r: {
-      preConfigure = r.preConfigure + ''
-        # Specify path to Überzug
-        substituteInPlace ranger/ext/img_display.py \
-          --replace "Popen(['ueberzug'" "Popen(['${pkgs.ueberzug}/bin/ueberzug'"
-
-        # Use Überzug as the default method
-        substituteInPlace ranger/config/rc.conf \
-          --replace 'set preview_images_method w3m' 'set preview_images_method ueberzug'
-        '';
-        })
-    ];
+  home.packages = with pkgs; [ ueberzug ranger ];
   xdg = {
     configFile = {
       "ranger".source = ./ranger;
