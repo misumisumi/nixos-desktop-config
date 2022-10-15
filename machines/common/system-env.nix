ac06bb2493e58bf11df265a9cc753695fd5ac379
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-15 14:49:28 +0900

    fix locale

--- machines/common/system-env.nix
+++ machines/common/system-env.nix
@@ -8,6 +8,10 @@ in
   time.timeZone = "Asia/Tokyo";             # Time zone and internationalisation
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+          "en_US.UTF-8/UTF-8"
+          "ja_JP.SJIS/SJIS"
+        ];
     extraLocaleSettings = {                 # Extra locale settings that need to be overwritten
       LC_TIME = "ja_JP.UTF-8";
       LC_MONETARY = "ja_JP.UTF-8";
