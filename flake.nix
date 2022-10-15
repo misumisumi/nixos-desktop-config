758716b8d263a1e5a3b752822a680f9c3241ed7e
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-15 22:13:30 +0900

    add version

--- flake.nix
+++ flake.nix
@@ -1,36 +1,35 @@
-let
-  stateVersion = "22.05";       # For Homa Manager
-  channelVersion = "unstable";  # For nixpkgs channel
-in
 {
   description = "Each my machine NixOS System Flake Configuration";
 
   inputs = 
-  {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-${channelVersion}";
+    let
+      channelVersion = "unstable";  # For nixpkgs channel
+    in
+    {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-${channelVersion}";
 
-    flake-utils.url = "github:numtide/flake-utils";
+      flake-utils.url = "github:numtide/flake-utils";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+
+      nur = {
+        url = "github:nix-community/NUR";
+      };
     };
-
-    nur = {
-      url = "github:nix-community/NUR";
-    };
-  };
 
   outputs = inputs @ {self, nixpkgs, flake-utils, home-manager, nur}: 
     let
       user = "sumi";
-      location = "$HOME/.dotfiles";
+      stateVersion = "22.05";       # For Homa Manager
     in
     { 
       nixosConfigurations = (
         import ./machines {
           inherit (nixpkgs) lib;
-          inherit inputs nixpkgs home-manager nur user location;
+          inherit inputs nixpkgs home-manager nur user stateVersion;
         }
       );
     };
