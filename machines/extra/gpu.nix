f5e487fcb72a693037a4681b5bfd2d9b0eed95af
Kobayashi Sumiharu <dragon511southern@gmai.com>
2022-10-30 11:52:15 +0900

    prime-run install from Sumi-Sumi/flakes

--- machines/extra/gpu.nix
+++ machines/extra/gpu.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
-let
-  prime-run = pkgs.writeShellScriptBin "prime-run" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in
+
 {
-  environment.systemPackages = [ prime-run ];
   services = {
     xserver = {
       videoDrivers = [
