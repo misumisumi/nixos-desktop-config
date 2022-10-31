d6fc29c714b56e2c34c545d688565c4f9d41e10e
Kobayashi Sumiharu <dragon511southern@gmai.com>
2022-10-31 15:39:09 +0900

    fix hardware-conf for zephyrus

--- machines/zephyrus/hardware-configuration.nix
+++ machines/zephyrus/hardware-configuration.nix
@@ -7,17 +7,19 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
-
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "uas" "sd_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
-  boot.initrd.luks.devices = {
-    luksroot = {
-      device = "/dev/disk/by-partlabel/LUKSROOT";
-      preLVM = true;
-      allowDiscards = true;
+  boot = {
+    initrd = {
+      availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "uas" "sd_mod" ];
+      availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "uas" "sd_mod" ];
+      luks.devices = {
+        luksroot = {
+          device = "/dev/disk/by-partlabel/Linux\x20LVM";
+          preLVM = true;
+          allowDiscards = true;
+        };
+      };
     };
+    kernelModules = [ "kvm-amd" ];
   };
 
   fileSystems."/" =
@@ -37,8 +39,16 @@
       device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
-
-  swapDevices = [ ];
+  # lower image_size
+  systemd.tmpfiles.rules = [
+    "w    /sys/power/image_size - - - - 0"
+  ];
+  swapDevices = [ 
+    {
+      device = "/.swapfile";
+      size = 1024 * 16;
+    }
+  ];
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
