621bf6adf79474d73bde8671c8c583c937348b7d
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-10-31 13:58:24 +0900

    fix default-user.nix

--- machines/common/default-user.nix
+++ machines/common/default-user.nix
@@ -7,8 +7,6 @@
 
   users.users.${user} = {
     isNormalUser = true;
-    home="/home/${user}";
-    createHome = true;
     shell = pkgs.zsh;
     initialHashedPassword = if user == "general"
       then "$6$HhoKyaJgibvjdoPO$swxNlcV6CJTxyKR/I.3o4a1utr2eSLgVjsogNKeo58jY/eFpRciLr8emsCmPxaSa5jwzHnz74RLlXEEYXIoQq/" # general
