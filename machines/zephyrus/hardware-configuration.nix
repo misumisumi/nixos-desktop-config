66311adbf6ddfcb8a4ab44544c9de6aa01cbb086
Kobayashi Sumiharu <dragon511southern@gmai.com>
2022-10-31 15:39:36 +0900

    fix hardware-conf for zephyrus

--- machines/zephyrus/hardware-configuration.nix
+++ machines/zephyrus/hardware-configuration.nix
@@ -4,9 +4,9 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports =[ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
   boot = {
     initrd = {
       availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "uas" "sd_mod" ];
