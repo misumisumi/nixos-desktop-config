2ef8645e751eed8250765c9dbe28a89fd8a8c835
Kobayashi Sumiharu <dragon511southern@gmai.com>
2022-10-31 13:15:10 +0900

    fix initialHashedPassword

--- machines/common/default-user.nix
+++ machines/common/default-user.nix
@@ -8,7 +8,6 @@
   users.users.${user} = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    initialHashedPassword = "$6$viPBN7o74kK3JdGw$4zKIuVEbgqvTqLIae/G5rOgrYXWSccB5MQp9/0HgeitQIocLg2.GeG7TWYYfNhZdgs4FNHJuPg5SqSrrIkpr51";
     initialHashedPassword = if users == "general"
       then "$6$HhoKyaJgibvjdoPO$swxNlcV6CJTxyKR/I.3o4a1utr2eSLgVjsogNKeo58jY/eFpRciLr8emsCmPxaSa5jwzHnz74RLlXEEYXIoQq/" # general
       else "$6$viPBN7o74kK3JdGw$4zKIuVEbgqvTqLIae/G5rOgrYXWSccB5MQp9/0HgeitQIocLg2.GeG7TWYYfNhZdgs4FNHJuPg5SqSrrIkpr51";
