78c75f7db54b65879f7d0ea0f1072b6b64a858d5
Kobayashi Sumiharu <dragon511southern@gmai.com>
2022-10-31 13:15:43 +0900

    fix variable

--- machines/common/default-user.nix
+++ machines/common/default-user.nix
@@ -8,7 +8,7 @@
   users.users.${user} = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    initialHashedPassword = if users == "general"
+    initialHashedPassword = if user == "general"
       then "$6$HhoKyaJgibvjdoPO$swxNlcV6CJTxyKR/I.3o4a1utr2eSLgVjsogNKeo58jY/eFpRciLr8emsCmPxaSa5jwzHnz74RLlXEEYXIoQq/" # general
       else "$6$viPBN7o74kK3JdGw$4zKIuVEbgqvTqLIae/G5rOgrYXWSccB5MQp9/0HgeitQIocLg2.GeG7TWYYfNhZdgs4FNHJuPg5SqSrrIkpr51";
     extraGroups = [ "wheel" "lxd" "libvirt" "uucp" "kvm" "input" "audio" "video" "scanner" "lp" ];
