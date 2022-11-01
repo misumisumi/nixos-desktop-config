e1d1c83d113d507c8620768712d69b035975cd0e
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-01 22:23:48 +0900

    fix path

--- apps/desktop/wm/qtile/xserver.nix
+++ apps/desktop/wm/qtile/xserver.nix
@@ -1,6 +1,6 @@
 { hostname, pkgs, ... }:
 let
-  png = if hostname != "general" then "${pkgs.my-wallpapers.background.png}" else "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
+  png = if hostname != "general" then "${pkgs.my-wallpapers}/wallpapers/background.png" else "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
 in
 {
   imports = [ ../common/xserver.nix ];
