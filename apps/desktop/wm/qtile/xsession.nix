4bfb4dccc23813eb957df3da0f18fd236d374c0a
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-11-01 16:09:34 +0900

    put wallpaper for qtile

--- apps/desktop/wm/qtile/xsession.nix
+++ apps/desktop/wm/qtile/xsession.nix
@@ -4,8 +4,11 @@ NixOS is not manager Keyboard if you use this, so you must manage xkb keyboard f
 However, mouse and trackpad are managed from xserver. (conf is ./xserver.nix)
 */
 
-{ config, lib, user, pkgs, ... }:
-
+{ config, lib, hostname, pkgs, ... }:
+with lig; 
+let
+  cfg = config.xsession.qtile.nixosWallpapers;
+in
 {
   home = {
     packages = with pkgs; [ qtile libinput-gestures ];
@@ -18,8 +21,20 @@ However, mouse and trackpad are managed from xserver. (conf is ./xserver.nix)
   };
   xsession = {
     putWallpapers.enable = true;
+    qtile.nixosWallpapers = true;
     windowManager = {             # Not launch using dbus-launch because systemd manage dbus-user-mesage since ver.226
       command = "qtile start";    # You maybe have some probrem (ex fcitx5...) if you launch using it.
     };                            # You can see this in ArchWiki https://wiki.archlinux.jp/index.php/Systemd/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC#D-Bus
+  };
+
+  options = {
+    xsession.qtile.nixosWallpapers = mkEnableOption ''
+        Get wallpaper from nixos-artwork.
+      '';
+  };
+  config = mkIf cfg.enable {
+    home = {
+      file."${config.home.homeDirectory}/Pictures/wallpapers/fixed/main.png".source = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
+    };
   };
 }
