b5fe650b8db2b31f7daf795dbd4ee7c928c98b99
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-11-01 16:26:51 +0900

    fix qtile wallper

---
+++ apps/desktop/wm/qtile/nixosWallpaper.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+with lib; 
+let
+  cfg = config.home.nixosWallpapers;
+in
+{
+  options = {
+    home.nixosWallpapers = {
+      enable = mkEnableOption ''
+        Get wallpaper from nixos-artwork.
+      '';
+    };
+  };
+  config = mkIf cfg.enable {
+    home = {
+      file."${config.home.homeDirectory}/Pictures/wallpapers/fixed/0_main.png".source = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
+      file."${config.home.homeDirectory}/Pictures/wallpapers/fixed/1_main.png".source = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
+      file."${config.home.homeDirectory}/Pictures/wallpapers/unfixed/main.png".source = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
+    };
+  };
+}
--- apps/desktop/wm/qtile/xsession.nix
+++ apps/desktop/wm/qtile/xsession.nix
@@ -4,15 +4,14 @@ NixOS is not manager Keyboard if you use this, so you must manage xkb keyboard f
 However, mouse and trackpad are managed from xserver. (conf is ./xserver.nix)
 */
 { config, lib, hostname, pkgs, ... }:
-with lib; 
 let
-  cfg = config.home.nixosWallpapers;
   use_my = if hostname != "general" then true else false;
 in
 {
+  imports = [ ./nixosWallpaper.nix ];
   home = {
     packages = with pkgs; [ qtile libinput-gestures ];
-    nixosWallpapers = use_my == false;
+    nixosWallpapers.enable = use_my == false;
   };
 
   xdg = {
@@ -25,18 +24,5 @@ in
     windowManager = {             # Not launch using dbus-launch because systemd manage dbus-user-mesage since ver.226
       command = "qtile start";    # You maybe have some probrem (ex fcitx5...) if you launch using it.
     };                            # You can see this in ArchWiki https://wiki.archlinux.jp/index.php/Systemd/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC#D-Bus
-  };
-
-  options = {
-    home.nixosWallpapers = mkEnableOption ''
-        Get wallpaper from nixos-artwork.
-      '';
-  };
-  config = mkIf cfg.nixosWallpapers {
-    home = {
-      file."${config.home.homeDirectory}/Pictures/wallpapers/fixed/0_main.png".source = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
-      file."${config.home.homeDirectory}/Pictures/wallpapers/fixed/1_main.png".source = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
-      file."${config.home.homeDirectory}/Pictures/wallpapers/unfixed/main.png".source = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
-    };
   };
 }
