b5d14621ecd080f9ff23da426e526d6a3474d688
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-04 09:00:05 +0900

    obs-ndi now optional

--- apps/desktop/media/obs-studio.nix
+++ apps/desktop/media/obs-studio.nix
@@ -1,23 +1,17 @@
 /*
 Streaming/Recording app
 */
-{ pkgs, ... }:
+{ lib, hostname, pkgs, ... }:
 
-{
+with lib; {
   programs = {
     obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [
-        obs-ndi
         looking-glass-obs
         obs-pipewire-audio-capture
-        # fetchFromGitHub {
-        #   owner = "dev47apps";
-        #   repo = "droidcam-obs-plugin";
-        #   rev = "1.6.0";
-        #   sha256 = "083pv2l8d1hnfz00y0dfaid87qxwzn23077ciz5z4rn118rxkfr0";
-        # }
-      ];
+      ] 
+      ++ optional (hostname != "general") obs-ndi;
     };
   };
 }
