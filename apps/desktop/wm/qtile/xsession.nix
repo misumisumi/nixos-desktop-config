8197210b9d480a2b5a1ba8b4384c63fb2529ffe1
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-11-09 00:05:33 +0900

    make modules dir

--- apps/desktop/wm/qtile/xsession.nix
+++ apps/desktop/wm/qtile/xsession.nix
@@ -6,7 +6,7 @@ However, mouse and trackpad are managed from xserver. (conf is ./xserver.nix)
 { config, lib, hostname, pkgs, ... }:
 
 with lib; {
-  imports = optional (hostname == "general") ./nixosWallpaper.nix;
+  imports = optional (hostname == "general") ../../../../modules/nixosWallpaper.nix;
   home = {
     packages = with pkgs; [ qtile libinput-gestures ];
   };
