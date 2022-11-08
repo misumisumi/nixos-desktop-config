baaa7949a5c80da7282f9e067395a9953d92ac7f
Kobayashi Sumiharu <dragon511southern@gmail.com>
2022-11-09 02:40:07 +0900

    add conf for yandere

---
+++ machines/yandere/network.nix
@@ -0,0 +1,34 @@
+{ hostname, ... }:
+
+{
+  imports = [
+    ../common/network.nix
+  ];
+  networking = {
+    hostName = "${hostname}";
+  };
+
+  systemd = {
+    network = {
+      lab-network.enable = true;
+      netdevs = {
+        "br0".netdevConfig = {
+          Kind = "bridge";
+          Name = "br0";
+        };
+      };
+      networks = {
+        "10-wired" = {
+          name = "enp4s0";
+          bridge = [ "br0" "univ" ];
+        };
+        "20-br0" = {
+          name = "br0";
+          dns = [ "192.168.1.40" "127.0.0.1" ];
+          address = [ "192.168.1.30" ];
+          gateway = [ "192.168.1.1" ];
+        };
+      };
+    };
+  };
+}
