e289f627a811a909639c1e764e5d6201b17bf049
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-15 23:34:52 +0900

    split overlays patches

---
+++ patches/default.nix
@@ -0,0 +1,12 @@
+[
+  (final: prev: {
+      python3Packages = prev.python3Packages.override {
+        overrides = pfinal: pprev: {
+          dbus-next = pprev.dbus-next.overridePythonAttrs (old: { # dbus-nest have issue in test so remove some test.
+            # temporary fix for https://github.com/NixOS/nixpkgs/issues/197408
+            checkPhase = builtins.replaceStrings ["not test_peer_interface"] ["not test_peer_interface and not test_tcp_connection_with_forwarding"] old.checkPhase;
+          });
+        };
+      };
+    })
+]
