889a422931463f1df32f0445e65b9c1fb8ef7840
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-15 23:45:43 +0900

    fix ovmf

--- apps/virtualisation/libvirt.nix
+++ apps/virtualisation/libvirt.nix
@@ -28,13 +28,16 @@ libvirt conf
       onBoot = "ignore";
       onShutdown = "shutdown";
       qemu = {
-        ovmf.packages = with pkgs; [ OVMFFull ];
-        verbatimConfig = ''
-          nvram = [ 
-            "${pkgs.OVMFFull}/FV/OVMF.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
-            "${pkgs.OVMFFull}/FV/OVMF.secboot.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
-          ]
-        '';
+        ovmf = {
+          enable = true;
+          packages = with pkgs; [ OVMFFull ];
+        };
+        # verbatimConfig = ''
+        #   nvram = [ 
+        #     "${pkgs.OVMFFull}/FV/OVMF.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
+        #     "${pkgs.OVMFFull}/FV/OVMF.secboot.fd:${pkgs.OVMFFull}/FV/OVMF_VARS.fd",
+        #   ]
+        # '';
       };
     };
     spiceUSBRedirection.enable = true;        # USB passthrough
