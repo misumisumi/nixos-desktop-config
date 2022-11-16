3ebdcb3861fed609e93c5c7192fb890f8f6a3dc1
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-16 22:27:33 +0900

    fix patch

--- patches/default.nix
+++ patches/default.nix
@@ -6,50 +6,14 @@
           # temporary fix for https://github.com/NixOS/nixpkgs/issues/197408
           checkPhase = builtins.replaceStrings ["not test_peer_interface"] ["not test_peer_interface and not test_tcp_connection_with_forwarding"] old.checkPhase;
         });
-        qtile = pprev.qtile.overrideAttrs (old: {
-          patches = old.patches ++ [
-            ./fix-xcbq.patch
-          ];
-        });
       };
     };
   })
-  # (final: prev: {
-  #     python3Packages = prev.python3Packages.override {
-  #       overrides = pfinal: pprev: {
-  #         qtile = pprev.qtile.overridePythonAttrs (old: { # dbus-nest have issue in test so remove some test.
-  #           patches = old.patches ++ [
-  #             ./fix-monitor.patch
-  #           ];
-  #           patchPhase = ''
-  #             ls ./
-  #             exit 1
-  #           '';
-
-  #         });
-  #       };
-  #     };
-  #   })
-  # (final: prev: {
-  #   qtile = prev.qtile.overrideAttrs (old: {
-  #     passthru.unwrapped = {
-  #       patches = old.passthru.unwrapped.patches ++ [
-  #         ./fix-monitor.patch
-  #       ];
-  #     };
-  #   });
-  #  })
-  # (final: prev: rec {
-  #   pythonOverrides = pfinal: pprev: {
-  #     qtile = pprev.qtile.overridePythonAttrs (old: {
-  #       patches = final.patches ++ [
-  #         ./fix-monitor.patch
-  #       ];
-  #     });
-  #   };
-  #   python3 = prev.python3.override {
-  #     packageOverrides = final.pythonOverrides;
-  #   };
-  # })
-  #  })
+  (final: prev: {
+    qtile = prev.qtile.unwrapped.override (old: {
+      patches = old.patches ++ [
+        ./fix-xcbq.patch
+      ];
+    });
+  })
 ]
