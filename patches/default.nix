982895250673b7f711760ab7e7ddb9031a8d195d
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-16 22:09:19 +0900

    fix patch

--- patches/default.nix
+++ patches/default.nix
@@ -1,20 +1,55 @@
 [
   (final: prev: {
-      python3Packages = prev.python3Packages.override {
-        overrides = pfinal: pprev: {
-          dbus-next = pprev.dbus-next.overridePythonAttrs (old: { # dbus-nest have issue in test so remove some test.
-            # temporary fix for https://github.com/NixOS/nixpkgs/issues/197408
-            checkPhase = builtins.replaceStrings ["not test_peer_interface"] ["not test_peer_interface and not test_tcp_connection_with_forwarding"] old.checkPhase;
-          });
-        };
+    python3Packages = prev.python3Packages.override {
+      overrides = pfinal: pprev: {
+        dbus-next = pprev.dbus-next.overridePythonAttrs (old: { # dbus-nest have issue in test so remove some test.
+          # temporary fix for https://github.com/NixOS/nixpkgs/issues/197408
+          checkPhase = builtins.replaceStrings ["not test_peer_interface"] ["not test_peer_interface and not test_tcp_connection_with_forwarding"] old.checkPhase;
+        });
+        qtile = pprev.qtile.overrideAttrs (old: {
+          patches = old.patches ++ [
+            ./fix-xcbq.patch
+          ];
+        });
       };
-    })
-  (final: prev: {
-    qtile = prev.qtile.overrideAttrs (old: {
-      patches = [
-        ./fix-restart.patch # https://github.com/NixOS/nixpkgs/issues/139568
-        ./fix-monitor.patch
-      ];
-    });
-   })
+    };
+  })
+  # (final: prev: {
+  #     python3Packages = prev.python3Packages.override {
+  #       overrides = pfinal: pprev: {
+  #         qtile = pprev.qtile.overridePythonAttrs (old: { # dbus-nest have issue in test so remove some test.
+  #           patches = old.patches ++ [
+  #             ./fix-monitor.patch
+  #           ];
+  #           patchPhase = ''
+  #             ls ./
+  #             exit 1
+  #           '';
+
+  #         });
+  #       };
+  #     };
+  #   })
+  # (final: prev: {
+  #   qtile = prev.qtile.overrideAttrs (old: {
+  #     passthru.unwrapped = {
+  #       patches = old.passthru.unwrapped.patches ++ [
+  #         ./fix-monitor.patch
+  #       ];
+  #     };
+  #   });
+  #  })
+  # (final: prev: rec {
+  #   pythonOverrides = pfinal: pprev: {
+  #     qtile = pprev.qtile.overridePythonAttrs (old: {
+  #       patches = final.patches ++ [
+  #         ./fix-monitor.patch
+  #       ];
+  #     });
+  #   };
+  #   python3 = prev.python3.override {
+  #     packageOverrides = final.pythonOverrides;
+  #   };
+  # })
+  #  })
 ]
