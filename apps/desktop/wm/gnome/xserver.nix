9fd0dde0f61df08fa7f148ac49a2d0cfe5e6cf52
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-18 17:42:09 +0900

    fix default session for gnome

--- apps/desktop/wm/gnome/xserver.nix
+++ apps/desktop/wm/gnome/xserver.nix
@@ -4,7 +4,6 @@
   imports = [ ../common/xserver.nix ];
 
   services.xserver.displayManager = {
-    defaultSession = "gnome";
     lightdm.background = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
   };
   services.xserver.desktopManager = {
