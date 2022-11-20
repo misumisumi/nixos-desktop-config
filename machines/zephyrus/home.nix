0885ba6619c5b34bdc8c6c3aa4f27e29d4693d12
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-20 17:41:06 +0900

    add powertop

--- machines/zephyrus/home.nix
+++ machines/zephyrus/home.nix
@@ -12,7 +12,8 @@
   home = {
     packages = (import ../../apps/common/pkgs) pkgs ++
                (import ../../apps/desktop/pkgs) pkgs ++
-               (import ../../apps/virtualisation/pkgs) pkgs;
+               (import ../../apps/virtualisation/pkgs) pkgs ++
+               (with pkgs; [ powertop ]);
   };
 
   xresources = {
