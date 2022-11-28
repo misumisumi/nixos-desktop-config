7fe749fe478e7a0861137ff94a868fe4016c2217
Sumi-Sumi <dragon511southern@gmail.com>
2022-11-28 15:26:59 +0900

    rename asus.nix to zephyrus.nix

--- machines/zephyrus/zephyrus.nix
+++ machines/zephyrus/zephyrus.nix
@@ -6,6 +6,14 @@
     asus-notify.enable = true;
     supergfxd.enable = true;
   };
+  systemd.sleep.extraConfig = ''
+    # suspend=hybrid-sleep
+    SuspendMode=suspend
+    SuspendState=disk
+    # hibernate=hybrid-sleep
+    HibernateMode=suspend
+    HibernateState=disk
+  '';
   powerManagement = {
     enable = true;
     powertop.enable = true;
