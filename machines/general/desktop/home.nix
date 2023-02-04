07146c5b8699a0d0b8ccb94b813e6692ad2edf8e
Sumi-Sumi <dragon511southern@gmail.com>
2023-02-04 22:15:42 +0900

    fix derivation

--- machines/general/desktop/home.nix
+++ machines/general/desktop/home.nix
@@ -8,13 +8,13 @@ with lib;
     (import (appDir + "/common/git")) ++
     (import (appDir + "/common/neovim")) ++
     (import (appDir + "/common/shell")) ++
-    (import (appDir + "/desktop") { inherit lib hostname; } // optionalAttrs (wm == "qtile") { isMidium = true; }) ++
+    (import (appDir + "/desktop") ({ inherit lib hostname; } // optionalAttrs (wm == "qtile") { isMidium = true; })) ++
     (import (appDir + "/desktop/wm/${wm}"));
 
   home = {
     packages = (import (appDir + "/common/pkgs") pkgs) ++
       # You can set "isLarge" and "addCLITools", if you want to office, sns and etc. Please see apps/desktop/pkgs/default.nix
-      (import (appDir + "/desktop/pkgs") { inherit lib pkgs; } // optionalAttrs (wm == "qtile") { isMidium = true; }) ++
+      (import (appDir + "/desktop/pkgs") ({ inherit lib pkgs; } // optionalAttrs (wm == "qtile") { isMidium = true; })) ++
       (with pkgs; [ pacman arch-install-scripts vscode ]);
   };
 
