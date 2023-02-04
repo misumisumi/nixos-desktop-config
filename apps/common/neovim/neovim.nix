de2162e49c304234729d773b989553c710a2ca48
Sumi-Sumi <dragon511southern@gmail.com>
2023-02-04 20:07:25 +0900

    remove old style neovim config

--- apps/common/neovim/neovim.nix
+++ apps/common/neovim/neovim.nix
@@ -1,47 +1,7 @@
-/*
-  neovim conf
-  I use conf sourcing ./nvim but you can manager from home manager.
-*/
+# neovim conf
+# I use conf sourcing ./nvim but you can manager from home manager.
 { config, pkgs, ... }:
 
 {
-
   programs.nvim-my-conf.enable = true;
-  # home.sessionVariables = {
-  #   SQLITE_CLIB_PATH = "${pkgs.sqlite.out}/lib/libsqlite3.so";
-  # };
-
-  # programs = {
-  #   neovim = {
-  #     enable = true; # Replace from vi&vim to neovim
-  #     viAlias = true;
-  #     vimAlias = true;
-  #     vimdiffAlias = true;
-
-  #     withNodeJs = true;
-  #     withPython3 = true;
-  #     withRuby = true;
-
-  #     extraPackages = with pkgs; [
-  #       go
-  #       cargo
-  #       ninja
-  #       gnumake
-  #       gcc # For nvim-treesitter
-  #       # zlib
-  #       patchelf
-  #       sqlite
-  #       yarn
-
-  #       ripgrep
-  #       silver-searcher # ToDO どっちか消す
-  #     ];
-
-  #     extraPython3Packages = ps: with ps; [
-  #       isort
-  #       docformatter
-  #       doq
-  #     ];
-  #   };
-  # };
 }
