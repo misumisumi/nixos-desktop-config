fc0451a54a430cd812ecc6edbf20def8211974a5
Sumi-Sumi <dragon511southern@gmail.com>
2023-03-23 19:40:46 +0900

    update: add nixos module for vfio-pci loading

--- machines/ordinary/mother/virtualisation.nix
+++ machines/ordinary/mother/virtualisation.nix
@@ -5,6 +5,7 @@
       IOMMUType = "amd";
       devices = [ ];
       # devices = [ "10de:2204" "10de:1aef" ];
+      deviceDomains = [ "0000:09:00.0" "0000:09:00.1" ];
       blacklistNvidia = false;
       disableEFIfb = false;
       ignoreMSRs = true;
@@ -18,23 +19,23 @@
     };
     scream.enable = true;
   };
-  boot.initrd = {
-    preDeviceCommands = ''
-      DEVS="0000:09:00.0 0000:09:00.1"
-      if [ -z "$(ls -A /sys/class/iommu)" ]; then
-        exit 0
-      fi
-      for DEV in $DEVS; do
-        echo "vfio-pci" > "/sys/bus/pci/devices/$DEV/driver_override"
-      done
-    '';
-  };
-  systemd.services.vfio-load = {
-    description = "Insert vfio-pci driver";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "/run/current-system/sw/bin/modprobe -i vfio-pci";
-    };
-  };
+  # boot.initrd = {
+  #   preDeviceCommands = ''
+  #     DEVS="0000:09:00.0 0000:09:00.1"
+  #     if [ -z "$(ls -A /sys/class/iommu)" ]; then
+  #       exit 0
+  #     fi
+  #     for DEV in $DEVS; do
+  #       echo "vfio-pci" > "/sys/bus/pci/devices/$DEV/driver_override"
+  #     done
+  #   '';
+  # };
+  # systemd.services.vfio-load = {
+  #   description = "Insert vfio-pci driver";
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = "/run/current-system/sw/bin/modprobe -i vfio-pci";
+  #   };
+  # };
 }
