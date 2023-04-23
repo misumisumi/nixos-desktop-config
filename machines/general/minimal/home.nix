bbc16109133557e479b472d45143122c4c9826d1
Sumi-Sumi <dragon511southern@gmail.com>
2023-04-23 12:29:43 +0900

    add(general/minimal/home): some pkgs

--- machines/general/minimal/home.nix
+++ machines/general/minimal/home.nix
@@ -1,4 +1,18 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   programs.neovim.useMyDots.enable = true;
   programs.editorconfig.useMyDots.enable = true;
+  home = {
+    installCommonPkgs = {
+      enable = true;
+    };
+    packages =
+      (import ../../../apps/userWide/pkgs {
+        inherit lib pkgs;
+      })
+      ++ (with pkgs; [pacman arch-install-scripts vscode]);
+  };
 }
