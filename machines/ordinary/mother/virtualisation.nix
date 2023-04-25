f8b47d5a77b61218a969c72415cd7fecff7948de
Sumi-Sumi <dragon511southern@gmail.com>
2023-04-26 02:38:15 +0900

    update(mother/virtualization): add hook

--- machines/ordinary/mother/virtualisation.nix
+++ machines/ordinary/mother/virtualisation.nix
@@ -10,6 +10,38 @@
       disableEFIfb = false;
       ignoreMSRs = true;
       applyACSpatch = false;
+      hook = {
+        enable = true;
+        preHook = {
+          win11 = ''
+            #! /usr/bin/env bash
+            echo "0000:09:00.0" >/sys/bus/pci/devices/0000:09:00.0/driver/unbind
+            echo "0000:09:00.1" >/sys/bus/pci/devices/0000:09:00.1/driver/unbind
+            nowDriver="$(lspci -s 0000:09:00.0 | grep "Kernel driver in use" | awk -F' ' '{print $5}')"
+            if [ "''${nowDriver}" != "nvidia" ]; then
+              echo "nvidia" >/sys/bus/pci/devices/0000:09:00.0/driver/driver_override
+              echo "0000:09:00.0" >/sys/bus/pci/devices/0000:09:00.0/driver/bind
+              echo "0000:09:00.1" >/sys/bus/pci/devices/0000:09:00.1/driver/bind
+              sleep 1
+              echo "0000:09:00.0" >/sys/bus/pci/devices/0000:09:00.0/driver/unbind
+              echo "0000:09:00.1" >/sys/bus/pci/devices/0000:09:00.1/driver/unbind
+            fi
+
+            virsh nodedev-detach pci_0000_09_00_0
+            virsh nodedev-detach pci_0000_09_00_1
+          '';
+        };
+        postHook = {
+          win11 = ''
+            #! /usr/bin/env bash
+            echo "0000:09:00.0" >/sys/bus/pci/devices/0000:09:00.0/driver/unbind
+            echo "0000:09:00.1" >/sys/bus/pci/devices/0000:09:00.1/driver/unbind
+
+            virsh nodedev-reattach pci_0000_09_00_0
+            virsh nodedev-reattach pci_0000_09_00_1
+          '';
+        };
+      };
     };
     hugepages = {
       enable = false;
