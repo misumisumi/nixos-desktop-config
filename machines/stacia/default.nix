faf30af3f5d8bc46db5e8ca4a4c8d632daa0a891
Sumi-Sumi <dragon511southern@gmail.com>
2023-04-26 14:22:58 +0900

    fix(stacia): fix path

--- machines/stacia/default.nix
+++ machines/stacia/default.nix
@@ -6,9 +6,9 @@
       ./gpu.nix
       ./virtualisation.nix
       ./own-system-conf.nix
-      ../../common/pipewire.nix
-      ../../common/musnix.nix
-      ../../common/printer.nix
+      ../common/pipewire.nix
+      ../common/musnix.nix
+      ../common/printer.nix
     ]
-    ++ (import ../../../apps/systemWide/wm/qtile);
+    ++ (import ../../apps/systemWide/wm/qtile);
 }
