9897b570d1f5ba553b32628ae8c09368b29641f1
Sumi-Sumi <dragon511southern@gmail.com>
2023-06-05 10:53:18 +0900

    update: add singularity

--- machines/mother/virtualisation.nix
+++ machines/mother/virtualisation.nix
@@ -1,4 +1,25 @@
 {
+  config,
+  pkgs,
+  ...
+}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      _singularity = prev.singularity.overrideAttrs (old: {
+        postFixup =
+          old.postFixup
+          + ''
+            ${prev.lib.optionalString true ''
+              substituteInPlace "$out/etc/singularity/singularity.conf" \
+                --replace "# nvidia-container-cli path =" "nvidia-container-cli path = ${prev.nvidia-docker}/bin/nvidia-container-cli"
+            ''}
+          '';
+      });
+      singularity = config.programs.singularity.packageOverriden;
+    })
+  ];
+  programs.singularity.enable = true;
+  programs.singularity.package = pkgs._singularity;
   virtualisation = {
     vfio = {
       enable = true;
