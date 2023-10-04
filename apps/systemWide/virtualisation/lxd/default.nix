33ffbee160300f7d04a23cf708626c52d769da77
misumisumi <dragon511southern@gmail.com>
2023-10-04 22:31:19 +0900

    fix(lxd): Place virtiofsd path in FHS compliant location for LXD-VM-agent

--- apps/systemWide/virtualisation/lxd/default.nix
+++ apps/systemWide/virtualisation/lxd/default.nix
@@ -1,4 +1,9 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
+
+  system.activationScripts.virtiofs.text = ''
+    mkdir -p /usr/lib/qemu
+    ln -sf ${pkgs.virtiofsd}/bin/virtiofsd /usr/lib/qemu/virtiofsd
+  '';
   virtualisation = {
     lxc.enable = true;
     lxd = {
