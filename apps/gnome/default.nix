2b6e9c7775c273888ef4caf30adf3f692fcd2c51
misumisumi <dragon511southern@gmail.com>
2023-12-08 13:52:18 +0900

    fix: background setting was exist in `xserver`

--- apps/gnome/default.nix
+++ apps/gnome/default.nix
@@ -6,9 +6,6 @@
 }: {
   imports = [ ../xserver ];
 
-  services.xserver.displayManager = {
-    lightdm.background = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath}";
-  };
   services.xserver.desktopManager = {
     gnome = {
       enable = true;
