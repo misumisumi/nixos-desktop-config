e47cc559e3619ee2acd217414fe1abfff27a6654
misumisumi <dragon511southern@gmail.com>
2024-01-22 22:39:47 +0900

    system/user: add `bash-completion` to pathsToLink

--- system/user/default.nix
+++ system/user/default.nix
@@ -6,7 +6,7 @@
 , pkgs
 , ...
 }: {
-  environment.pathsToLink = [ "/share/zsh" ];
+  environment.pathsToLink = [ "/share/zsh" "/share/bash-completion" ];
   programs.zsh = {
     enable = true;
     enableCompletion = false;
