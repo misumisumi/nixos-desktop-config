88e5f158f6e0d06da3b3c8abda65555279d143f0
misumisumi <dragon511southern@gmail.com>
2024-01-22 20:51:36 +0900

    mother: migrate to disko

--- machines/mother/hardware-configuration.nix
+++ machines/mother/hardware-configuration.nix
@@ -12,44 +12,7 @@
   boot = {
     initrd = {
       availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "uas" "sd_mod" ];
-      kernelModules = [ "dm-snapshot" ];
-      luks.devices = {
-        luksroot = {
-          device = "/dev/disk/by-partlabel/LUKSROOT";
-          preLVM = true;
-          allowDiscards = true;
-        };
-      };
     };
   };
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/mother-root";
-      fsType = "ext4";
-    };
-    "/nix" = {
-      device = "/dev/disk/by-label/mother-nix";
-      fsType = "ext4";
-    };
-    "/var" = {
-      device = "/dev/disk/by-label/mother-var";
-      fsType = "ext4";
-    };
-    "/home" = {
-      device = "/dev/disk/by-label/mother-home";
-      fsType = "ext4";
-    };
-    "/boot" = {
-      device = "/dev/disk/by-label/mo-boot";
-      fsType = "vfat";
-    };
-  };
-  swapDevices = [
-    {
-      device = "/dev/mapper/VolGroup00-lvolswap";
-      priority = 10;
-    }
-  ];
-
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
