bb3129e488a989d40a867234125bc5dd4fdfea48
misumisumi <dragon511southern@gmail.com>
2024-01-22 22:38:46 +0900

    liveimg: fix config

--- machines/liveimg/iso.nix
+++ machines/liveimg/iso.nix
@@ -1,8 +1,13 @@
 { lib
+, pkgs
 , modulesPath
 , ...
 }:
 {
   imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
   boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  isoImage.squashfsCompression = "zstd -Xcompression-level 6";
+  environment.systemPackages = with pkgs; [
+    neovim
+  ];
 }
