bc02e046126673d3c91ee740e44d404182e329ca
misumisumi <dragon511southern@gmail.com>
2024-03-18 00:29:20 +0900

    update: 検証用のネットワークセグメントのbridgeの作成

--- machines/mother/network.nix
+++ machines/mother/network.nix
@@ -11,8 +11,7 @@
     firewall = {
       enable = true;
       trustedInterfaces = [
-        "br0"
-        "lxdbr0"
+        "incusbr0"
         "k8sbr0"
       ];
       allowedUDPPorts = [
@@ -51,7 +50,6 @@
           netdevConfig = {
             Kind = "bridge";
             Name = "br0";
-            MACAddress = "56:47:6a:94:2d:34";
           };
           extraConfig = ''
             [Bridge]
@@ -59,29 +57,110 @@
             DefaultPVID=1
           '';
         };
+        "br1" = {
+          netdevConfig = {
+            Kind = "bridge";
+            Name = "br1";
+          };
+          extraConfig = ''
+            [Bridge]
+            VLANFiltering=1
+            DefaultPVID=100
+          '';
+        };
+        home = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "home";
+            MACAddress = "56:47:6a:94:2d:34";
+          };
+          vlanConfig = {
+            Id = 1;
+          };
+        };
+        dev-server-host = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "dev-server-host";
+            MACAddress = "7f:11:b1:60:a4:74";
+          };
+          vlanConfig = {
+            Id = 100;
+          };
+        };
+        dev-k8s = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "dev-server-host";
+            MACAddress = "7f:11:b1:60:a4:84";
+          };
+          vlanConfig = {
+            Id = 101;
+          };
+        };
+        dev-nfs = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "dev-server-host";
+            MACAddress = "7f:11:b1:60:a4:94";
+          };
+          vlanConfig = {
+            Id = 102;
+          };
+        };
       };
       networks = {
-        "20-wired" = {
+        "10-wired" = {
           name = "enp5s0";
           bridge = [ "br0" ];
+        };
+        "20-br0" = {
+          name = "br0";
+          vlan = [ "home" "dev" ];
           bridgeVLANs = [
             {
               bridgeVLANConfig = {
-                VLAN = "10";
-                PVID = 1;
-                EgressUntagged = 1;
+                VLAN = "1";
               };
             }
             {
               bridgeVLANConfig = {
-                VLAN = "30";
+                VLAN = "100-102";
               };
             }
           ];
         };
-        "30-br0" = {
-          name = "br0";
+        "30-home" = {
+          name = "home";
           DHCP = "yes";
+        };
+        "40-dev-server-host" = {
+          name = "dev-server-host";
+          bridge = [ "br1" ];
+          DHCP = "no";
+        };
+        "40-dev-k8s" = {
+          name = "dev-k8s";
+          bridge = [ "br1" ];
+          DHCP = "no";
+        };
+        "40-dev-nfs" = {
+          name = "dev-nfs";
+          bridge = [ "br1" ];
+          DHCP = "no";
+        };
+        "50-br1" = {
+          name = "br1";
+          DHCP = "no";
+          bridgeVLANs = [
+            {
+              bridgeVLANConfig = {
+                VLAN = "100-102";
+                PVID = 100;
+                EgressUntagged = 100;
+              };
+            }
+          ];
         };
       };
     };
