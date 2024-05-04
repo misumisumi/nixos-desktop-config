562fbdf62c2022410bc98fd0c7392b8454a8654d
misumisumi <dragon511southern@gmail.com>
2024-05-04 11:49:27 +0900

    fix(ssh): remove branching by version

--- apps/ssh/default.nix
+++ apps/ssh/default.nix
@@ -1,28 +1,20 @@
 { config
-, lib
-, stateVersion
 , ...
 }: {
   programs.ssh = {
     askPassword = "";
   };
   networking.firewall.allowedTCPPorts = config.services.openssh.ports;
-  services.openssh =
-    {
-      enable = true;
-      ports = [ 12511 ];
-      extraConfig = ''
-        UsePAM yes
-      '';
-    }
-    // lib.attrsets.optionalAttrs (stateVersion <= "22.11") {
-      kbdInteractiveAuthentication = true;
-      forwardX11 = true;
-    }
-    // lib.attrsets.optionalAttrs (stateVersion > "22.11") {
-      settings = {
-        KbdInteractiveAuthentication = true;
-        X11Forwarding = true;
-      };
+  services.openssh = {
+    enable = true;
+    ports = [ 12511 ];
+    extraConfig = ''
+      UsePAM yes
+    '';
+
+    settings = {
+      KbdInteractiveAuthentication = true;
+      X11Forwarding = true;
     };
+  };
 }
