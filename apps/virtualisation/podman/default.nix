a7673d328ab22aaf3624c6d6edc83a9f3ef03ccf
misumisumi <dragon511southern@gmail.com>
2024-05-04 11:50:42 +0900

    fix(nvidia-container-toolkit): rename option to `hardware.nvidia-container-toolkit`

--- apps/virtualisation/podman/default.nix
+++ apps/virtualisation/podman/default.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   virtualisation = {
-    containers.cdi.dynamic.nvidia.enable = true;
     podman = {
       enable = true;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
