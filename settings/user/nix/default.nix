3638e512e6c1223e11474687e160ae1287dda11a
misumisumi <dragon511southern@gmail.com>
2024-08-26 11:04:36 +0900

    fix(nix): fix cache config

--- settings/user/nix/default.nix
+++ settings/user/nix/default.nix
@@ -4,19 +4,27 @@
     settings = {
       auto-optimise-store = true; # Optimise syslinks
       # flakeの有効化
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       # ビルド時の依存関係を維持(オフラインでも再ビルド可能にする)
       keep-outputs = true;
       keep-derivations = true;
       substituters = [
+        "https://cache.nixos.org/"
         "https://misumisumi.cachix.org"
         "https://cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "misumisumi.cachix.org-1:f+5BKpIhAG+00yTSoyG/ihgCibcPuJrfQL3M9qw1REY="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
-      trusted-users = [ "root" "${user}" ];
+      trusted-users = [
+        "root"
+        "${user}"
+      ];
     };
     gc = {
       # 1週間ごとに7日前のイメージを削除
