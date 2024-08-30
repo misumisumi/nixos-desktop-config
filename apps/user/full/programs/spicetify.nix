d9e7100746de3f2478de8f08341b03acba0e9acb
misumisumi <dragon511southern@gmail.com>
2024-08-30 18:18:12 +0900

    fix(spicetify): remove patch

--- apps/user/full/programs/spicetify.nix
+++ apps/user/full/programs/spicetify.nix
@@ -1,32 +1,27 @@
-{ inputs
-, pkgs
-, ...
-}:
+{ inputs, pkgs, ... }:
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in
 {
-  programs.spicetify =
-    {
-      enable = true;
-      theme = spicePkgs.themes.catppuccin;
-      colorScheme = "mocha";
+  programs.spicetify = {
+    enable = true;
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
 
-      enabledExtensions = with spicePkgs.extensions; [
-        copyToClipboard
-        fullAppDisplay
-        hidePodcasts
-        history
-        keyboardShortcut
-        songStats
-        {
-          src = pkgs.fetchgit {
-            url = "https://github.com/L3-N0X/spicetify-dj-info";
-            rev = "50f417c554529c54ab7d4b398d505beca94ef417";
-            hash = "sha256-1jT+eISut0HkBVLMOWi2IpHMG2KSBnM3TzJbeSe5hKc=";
-          };
-          name = "djinfo.js";
-        }
-      ];
-    };
+    enabledExtensions = with spicePkgs.extensions; [
+      copyToClipboard
+      hidePodcasts
+      history
+      keyboardShortcut
+      songStats
+      {
+        src = pkgs.fetchgit {
+          url = "https://github.com/L3-N0X/spicetify-dj-info";
+          rev = "b55c9b8c8b00100b42e562829c77d012191f0c07";
+          hash = "sha256-cR1EndjBN7jGtD0zA1f/vjkjgqslBxTiNAGDxBGEYdE=";
+        };
+        name = "djinfo.js";
+      }
+    ];
+  };
 }
--- patches/default.nix
+++ patches/default.nix
@@ -41,8 +41,7 @@
 # })
 # Patch from https://github.com/NixOS/nixpkgs/pull/211600
 { nixpkgs-stable, ... }:
-final: prev:
-{
+final: prev: {
   # xp-pentablet-unwrapped = prev.libsForQt5.callPackage ./xp-pen-drivers { };
   haskellPackages = prev.haskellPackages.override {
     overrides = hself: hsuper: {
@@ -50,29 +49,5 @@ final: prev:
       thumbnail = prev.haskell.lib.doJailbreak hsuper.thumbnail;
     };
   };
-  # INFO: NixOS/nixpkgs#321738 がマージされるまでのパッチ
-  spicetify-cli = prev.callPackage "${prev.path}/pkgs/by-name/sp/spicetify-cli/package.nix" {
-    buildGoModule = args: prev.buildGoModule (args // rec {
-      version = "2.36.13";
-
-      src = prev.fetchFromGitHub {
-        owner = "spicetify";
-        repo = "cli";
-        rev = "v${version}";
-        hash = "sha256-0etyVzYL8F1GOAHEcpSfOoKe3GsGmAqVufVauqPDV1w=";
-      };
-
-      vendorHash = "sha256-po0ZrIXtyK0txK+eWGZDEIGMI1/cwyLVsGUVnTaHKP0=";
-      postInstall = ''
-        mv $out/bin/cli $out/bin/spicetify
-        ln -s $out/bin/spicetify $out/bin/spicetify-cli
-        cp -r ${src}/jsHelper $out/bin/jsHelper
-        cp -r ${src}/CustomApps $out/bin/CustomApps
-        cp -r ${src}/Extensions $out/bin/Extensions
-        cp -r ${src}/Themes $out/bin/Themes
-      '';
-    });
-  };
   xp-pentablet = prev.libsForQt5.callPackage ./xp-pen-drivers.nix { };
 }
-
