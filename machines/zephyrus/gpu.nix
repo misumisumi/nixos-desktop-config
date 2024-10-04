e51a909bfb9f4edfbfd64b7ff27ff499509478ae
misumisumi <dragon511southern@gmail.com>
2024-10-04 14:28:52 +0900

    fix(zephyrus): add prime run

--- machines/zephyrus/gpu.nix
+++ machines/zephyrus/gpu.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 {
   boot.kernelParams = [ "amdgpu.ppfeaturemask=0xfff7ffff" ];
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec "$@"
+    '')
+  ];
+
   services = {
     xserver = {
       videoDrivers = [
@@ -18,10 +28,7 @@
 
   hardware = {
     nvidia-container-toolkit.enable = true;
-    nvidia = {
-      prime.offload.enableOffloadCmd = true;
-      open = false;
-    };
+    nvidia.open = false;
     graphics.extraPackages = with pkgs; [
       libvdpau-va-gl
       vaapiVdpau
