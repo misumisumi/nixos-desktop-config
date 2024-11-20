c59757f8b5186a08c8e9a11ac98ad19eab13b6cc
misumisumi <dragon511southern@gmail.com>
2024-11-21 02:43:32 +0900

    fix(nix): remove nix-community cache

--- settings/user/nix/default.nix
+++ settings/user/nix/default.nix
@@ -14,12 +14,14 @@
       substituters = [
         "https://misumisumi.cachix.org"
         "https://cuda-maintainers.cachix.org"
-        "https://nix-community.cachix.org"
+        # "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
       ];
       trusted-public-keys = [
         "misumisumi.cachix.org-1:f+5BKpIhAG+00yTSoyG/ihgCibcPuJrfQL3M9qw1REY="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       trusted-users = [
         "root"
