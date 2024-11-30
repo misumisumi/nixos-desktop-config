cc2c8214688cf1b07f775d9b2d3ecc8ca654321b
misumisumi <dragon511southern@gmail.com>
2024-11-30 19:54:09 +0900

    fix: fix tool configs

--- apps/user/cli/develop/fastfetch/default.nix
+++ apps/user/cli/develop/fastfetch/default.nix
@@ -1,3 +1,6 @@
 {
+  home.shellAliases = {
+    neofetch = "fastfetch -c neofetch.jsonc";
+  };
   programs.fastfetch.enable = true;
 }
--- apps/user/cli/tool/utils/default.nix
+++ apps/user/cli/tool/utils/default.nix
@@ -6,7 +6,6 @@
     };
     packages = with pkgs; [
       ascii-image-converter # Make ascii art
-      cava # Console-based Audio Visualizer for Alsa
       cmatrix # Lain of character
       figlet # Make AA from character
       tty-clock # CLI clock
