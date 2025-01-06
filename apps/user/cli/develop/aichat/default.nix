bbf3412eea5a2eb5130dba4331c05320a12740d6
misumisumi <dragon511southern@gmail.com>
2025-01-06 19:49:42 +0900

    fix(aichat): fix completion url

--- apps/user/cli/develop/aichat/default.nix
+++ apps/user/cli/develop/aichat/default.nix
@@ -3,7 +3,7 @@
   home.packages = [ pkgs.aichat ];
   programs.zsh.zinit.plugins = {
     "wait'1c' lucid" = [
-      "atload'. ./aichat.zsh' https://raw.githubusercontent.com/sigoden/aichat/main/scripts/completions/aichat.zsh"
+      "atload'. ./aichat.zsh' https://raw.githubusercontent.com/sigoden/aichat/refs/heads/main/scripts/completions/aichat.zsh"
     ];
   };
   xdg.configFile."aichat/roles" = {
