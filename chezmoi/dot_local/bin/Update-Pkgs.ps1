b3709f132f5ed41ccdfe038681720c00ba784e62
misumisumi <sumiharu@misumi-sumi.com>
2025-02-05 23:54:39 +0900

    feat(chezmoi): support chocolatey package

--- chezmoi/dot_local/bin/Update-Pkgs.ps1
+++ chezmoi/dot_local/bin/Update-Pkgs.ps1
@@ -1,16 +1,21 @@
 Write-Host "Update all installed packages..."
 
 if (Get-Command * | Where-Object { $_.Name -match "winget" }) {
-Write-Host "Updating winget packages..."
-sudo winget upgrade `
-    --all `
-    --accept-package-agreements `
-    --accept-source-agreements `
-    --disable-interactivity `
-    --include-unknown
+    Write-Host "Updating winget packages..."
+    sudo winget upgrade `
+        --all `
+        --accept-package-agreements `
+        --accept-source-agreements `
+        --disable-interactivity `
+        --include-unknown
+}
+
+if (Get-Command * | Where-Object { $_.Name -match "choco" }) {
+    Write-Host "Updating chocolatey packages..."
+    sudo choco -y upgrade all
 }
 
 if (Get-Command * | Where-Object { $_.Name -match "scoop" }) {
-Write-Host "Updating scoop packages..."
-scoop update *
+    Write-Host "Updating scoop packages..."
+    scoop update *
 }
