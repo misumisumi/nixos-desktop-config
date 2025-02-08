bb9f81f5cb9624971cc5460ea96ed939184fb9e3
misumisumi <sumiharu@misumi-sumi.com>
2025-02-09 01:37:33 +0900

    fix(zathura): adding syntax settings
    [skip ci]

--- apps/user/desktop/tool/office/zathura/default.nix
+++ apps/user/desktop/tool/office/zathura/default.nix
@@ -3,6 +3,8 @@
     enable = true;
     extraConfig = ''
       set selection-clipboard clipboard
+      set synctex true
+      set synctex-editor-command "nvr --remote-silent +%{line} %{input}"
     '';
   };
 }
