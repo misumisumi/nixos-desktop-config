b16cc0fe8601a0befd922d6017303feba22463db
misumisumi <sumiharu@misumi-sumi.com>
2025-05-15 19:54:17 +0900

    fix(chezmoi): add missing pkgs and update sops

--- chezmoi/.chezmoiscripts/unix/run_once_after_10-ssh-keygen.sh
+++ chezmoi/.chezmoiscripts/unix/run_once_after_10-ssh-keygen.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-[ -d ~/.ssh ] || mkdir "$HOME/.ssh"
-[ -f ~/.ssh/id_ed25519.pub ] || ssh-keygen -t ed25519 -N "" -C "$(whoami)@$(hostname)-$(date -I)" -f "$HOME/.ssh/id_ed25519"
-[ -f ~/.ssh/id_rsa.pub ] || ssh-keygen -t rsa -b 4096 -N "" -C "$(whoami)@$(hostname)-$(date -I)" -f "$HOME/.ssh/id_rsa"
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_ed25519
-chmod 600 ~/.ssh/id_rsa
+[ -d ~/.ssh ] || mkdir "$HOME/.ssh" && chmod 700 ~/.ssh
+[ -f ~/.ssh/id_ed25519.pub ] || ssh-keygen -t ed25519 -N "" -C "$(whoami)@$(hostname)-$(date -I)" -f "$HOME/.ssh/id_ed25519" && chmod 600 ~/.ssh/id_ed25519
+[ -f ~/.ssh/id_rsa.pub ] || ssh-keygen -t rsa -b 4096 -N "" -C "$(whoami)@$(hostname)-$(date -I)" -f "$HOME/.ssh/id_rsa" && chmod 600 ~/.ssh/id_rsa
 
 # if executing go, run "go run"
-if [ -f ~/.age-key.txt ]; then
+if [ ! -f ~/.age-key.txt ]; then
   echo "Generate age key..."
   if command -v go &>/dev/null; then
-    go run "github.com/Mic92/ssh-to-age/cmd/ssh-to-age@latest" -private-key -i ~/.ssh/id_ed25519 -o "$HOME/.age-key.txt"
+    mkdir -p "$XDG_CONFIG_HOME/sops/age"
+    go run "github.com/Mic92/ssh-to-age/cmd/ssh-to-age@latest" -private-key -i ~/.ssh/id_ed25519 -o "$XDG_CONFIG_HOME/sops/age/keys.txt"
   fi
 fi
--- users/sumi/default.nix
+++ users/sumi/default.nix
@@ -10,7 +10,7 @@
     };
     age = {
       generateKey = true;
-      keyFile = "${config.home.homeDirectory}/.age-key.txt";
+      keyFile = "${config.xdg.configHome}}/sops/age/keys.txt";
       sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
     };
     secrets = {
