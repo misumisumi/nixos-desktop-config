7e6e7588a7aa5284e80ca8302e9482bdb1355abd
misumisumi <sumiharu@misumi-sumi.com>
2025-06-07 17:13:02 +0900

    fix(zotero): do not use the system document viewer

--- apps/user/desktop/tool/develop/zotero/default.nix
+++ apps/user/desktop/tool/develop/zotero/default.nix
@@ -5,7 +5,6 @@
     profiles."${user}" = {
       settings = {
         "extensions.zotero.automaticTags" = false; # disable automatic tags
-        "extensions.zotero.fileHandler.pdf" = "system"; # always open PDFs with the system viewer
         "extensions.zotero.ZoteroPDFTranslate.dictSource" = "webliodict";
         "extensions.zotero.ZoteroPDFTranslate.targetLanguage" = "ja";
         "extensions.zotero.ZoteroPDFTranslate.translateSource" = "googleapi";
