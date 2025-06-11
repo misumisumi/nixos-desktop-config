ab5df16a1f95d5bda3cbd9f9b62edeab4c1072f9
misumisumi <sumiharu@misumi-sumi.com>
2025-06-11 18:19:29 +0900

    fix(console): full path to font because systemd-vconsole-setup failed on stage 1

--- settings/system/console/default.nix
+++ settings/system/console/default.nix
@@ -1,6 +1,8 @@
+{ pkgs, ... }:
 {
   console = {
-    font = "Lat2-Terminus16";
+    # font = "Lat2-Terminus16";
+    font = "${pkgs.kbd}/share/consolefonts/Lat2-Terminus16.psfu.gz";
     useXkbConfig = true;
   };
 }
