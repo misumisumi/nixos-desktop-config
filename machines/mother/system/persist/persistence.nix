0be1db8737faf9c6539b45e7b67e01743e3e0151
misumisumi <sumiharu@misumi-sumi.com>
2025-06-23 11:59:17 +0900

    fix(btrfs): temporary response to the inability to use the trash bin when bind mounting with gnome
    - https://gitlab.gnome.org/GNOME/glib/-/issues/2382
    - https://github.com/nix-community/impermanence/issues/147#issuecomment-2552700123

--- machines/mother/system/persist/persistence.nix
+++ machines/mother/system/persist/persistence.nix
@@ -1,5 +1,55 @@
-{ user, ... }:
 {
+  user,
+  ...
+}:
+let
+  persistUserDirs = [
+    ".config"
+    ".local/share"
+    ".mozilla"
+    ".thunderbird"
+    ".vst3"
+    ".zinit"
+    ".zoom"
+    ".zotero"
+    "Unity"
+    "Documents"
+    "Music"
+    "Pictures"
+    "Templates"
+    "Videos"
+    "Workspace"
+    "Zotero"
+    {
+      directory = ".gnupg";
+      mode = "0700";
+    }
+    {
+      directory = ".ssh";
+      mode = "0700";
+    }
+    # {
+    #   directory = ".nixops";
+    #   mode = "0700";
+    # }
+  ];
+  persistAltUserDirs = [
+    ".cache"
+    ".local/share/Steam"
+    ".local/state"
+    "Downloads"
+    "go"
+  ];
+  addTrashOption = map (x: {
+    name =
+      if ((builtins.typeOf x) == "set") then "/home/${user}/${x.directory}" else "/home/${user}/${x}";
+    value = {
+      options = [ "x-gvfs-trash" ];
+    };
+  }) (persistUserDirs ++ persistAltUserDirs);
+in
+{
+  fileSystems = builtins.listToAttrs addTrashOption;
   environment.persistence."/nix/persist" = {
     enable = true;
     hideMounts = true;
@@ -11,36 +61,7 @@
       "/etc/machine-id"
     ];
     users.${user} = {
-      directories = [
-        ".config"
-        ".local/share"
-        ".mozilla"
-        ".thunderbird"
-        ".vst3"
-        ".zinit"
-        ".zoom"
-        ".zotero"
-        "Unity"
-        "Documents"
-        "Music"
-        "Pictures"
-        "Templates"
-        "Videos"
-        "Workspace"
-        "Zotero"
-        {
-          directory = ".gnupg";
-          mode = "0700";
-        }
-        {
-          directory = ".ssh";
-          mode = "0700";
-        }
-        # {
-        #   directory = ".nixops";
-        #   mode = "0700";
-        # }
-      ];
+      directories = persistUserDirs;
       # files = [ ];
     };
   };
@@ -51,13 +72,7 @@
       "/root/.local/share/nix"
     ];
     users.${user} = {
-      directories = [
-        ".cache"
-        ".local/share/Steam"
-        ".local/state"
-        "Downloads"
-        "go"
-      ];
+      directories = persistAltUserDirs;
     };
   };
 }
