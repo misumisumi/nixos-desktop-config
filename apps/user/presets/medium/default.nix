b9042cca70f0468351fdb6353a96ed4bc3c5d4d4
misumisumi <s.kobayashi@misumi-sumi.com>
2025-12-08 19:05:39 +0900

    fix: fix config including presets and update sops

--- apps/user/presets/medium/default.nix
+++ apps/user/presets/medium/default.nix
@@ -1,7 +1,15 @@
 {
   imports = [
     ../../cli/common
-    ../../cli/develop
+    ../../cli/develop/direnv
+    ../../cli/develop/editorconfig
+    ../../cli/develop/fastfetch
+    ../../cli/develop/lazygit
+    ../../cli/develop/navi
+    ../../cli/develop/neovim
+    ../../cli/develop/pkgs
+    ../../cli/develop/translate-shell
+    ../../cli/develop/yaskkserv2
     ../../cli/tool/multimedia
     ../../cli/tool/utils
     ../../core
