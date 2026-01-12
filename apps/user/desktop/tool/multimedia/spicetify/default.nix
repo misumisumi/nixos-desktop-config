5e2755bd95e1ea5683054ab871d6b4bf9fa70319
misumisumi <s.kobayashi@misumi-sumi.com>
2026-01-12 21:59:38 +0900

    chore(spicetify): update repo

--- apps/user/desktop/tool/multimedia/spicetify/default.nix
+++ apps/user/desktop/tool/multimedia/spicetify/default.nix
@@ -1,6 +1,5 @@
 {
   inputs,
-  pkgs,
   system,
   ...
 }:
@@ -18,13 +17,15 @@ in
       keyboardShortcut
       songStats
       {
-        src = pkgs.fetchgit {
-          url = "https://github.com/L3-N0X/spicetify-dj-info";
-          rev = "c786a923eee564cff3fa5c5e0317b34cab7c80f3";
-          hash = "sha256-YRkLXJivnOuy4dLZ5qutyTf0NK05Zeiy676HU2FWC0A=";
+        src = fetchFromGitHub {
+          owner = "L3-N0X";
+          repo = "spicetify-dj-info";
+          rev = "9f5ef20697aa5613490c21278889fb007c594610";
+          sha256 = "sha256-FbDgfbrvM+Sz7/CgRdtB6oOMndlnb2SPsNFosuV7+AY=";
         };
-        name = "djinfo.js";
+        name = "dist/djinfo.mjs";
       }
+
     ];
   };
 }
