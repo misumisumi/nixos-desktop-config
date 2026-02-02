fc9ed6aac55645e7ad8ab271eee9bf690eb716d3
misumisumi <s.kobayashi@misumi-sumi.com>
2026-02-02 14:27:38 +0900

    fix(onlyoffice): add activation script from NixOS/nixpkgs#373521
    - OnlyOffice cannot find symlink file.

--- apps/user/desktop/tool/office/onlyoffice/default.nix
+++ apps/user/desktop/tool/office/onlyoffice/default.nix
@@ -1,3 +1,4 @@
+{ lib, pkgs, ... }:
 {
   programs.onlyoffice = {
     enable = true;
@@ -8,4 +9,24 @@
       titlebar = "custom";
     };
   };
+  home.activation.copyFontsForOnlyOffice =
+    let
+      inherit (lib) concatMapStringsSep;
+      fonts = with pkgs; [
+        corefonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-color-emoji
+      ];
+    in
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      rm -rf ~/.local/share/fonts
+      mkdir -p ~/.local/share/fonts
+      font_regexp='.*\.\(ttf\|ttc\|otf\|pcf\|pfa\|pfb\|bdf\)\(\.gz\)?'
+      ${concatMapStringsSep "\n" (
+        x: "find ${x} -regex $font_regexp -exec cp '{}' ~/.local/share/fonts/ \\;"
+      ) fonts}
+      chmod 544 ~/.local/share/fonts
+      chmod 444 ~/.local/share/fonts/*
+    '';
 }
