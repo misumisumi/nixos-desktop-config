dd043daf45da7e0cfa411aadf9d3195865651eb1
misumisumi <s.kobayashi@misumi-sumi.com>
2026-03-22 11:44:35 +0900

    chore(flake.lock): update flakes

--- flake.nix
+++ flake.nix
@@ -55,8 +55,7 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # flakes.url = "github:misumisumi/flakes/feat/qemu-patch";
-    flakes.url = "path:/home/sumi/Workspace/nix/desktop/flakes";
+    flakes.url = "github:misumisumi/flakes";
     nvimdots = {
       url = "github:misumisumi/nvimdots";
       inputs = {
