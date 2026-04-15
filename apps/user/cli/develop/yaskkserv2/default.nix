f6d84922fd455b7b66fc0733360c007ac21d2fff
misumisumi <s.kobayashi@misumi-sumi.com>
2026-04-15 20:12:15 +0900

    fix(yaskkserv2): update config

--- apps/user/cli/develop/yaskkserv2/default.nix
+++ apps/user/cli/develop/yaskkserv2/default.nix
@@ -1,4 +1,17 @@
 # light weight skk server
+{ lib, config, ... }:
+let
+  inherit (lib) optional;
+in
 {
-  services.yaskkserv2.enable = true;
+  services.yaskkserv2 = {
+    enable = true;
+    extraArgs = [
+      "--google-japanese-input notfound"
+      "--google-cache-filename=/tmp/yaskkserv2.cache"
+    ]
+    ++ optional (
+      config.i18n.inputMethod.enable && config.i18n.inputMethod.type == "fcitx5"
+    ) "--midashi-utf8";
+  };
 }
