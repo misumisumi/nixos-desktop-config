a51a563377cecc2af28b017c3816eaec6be8b617
misumisumi <s.kobayashi@misumi-sumi.com>
2026-05-07 11:33:08 +0900

    fix(ssh): add ssh keys

--- users/sumi/default.nix
+++ users/sumi/default.nix
@@ -30,6 +30,31 @@
         sopsFile = getEncryptFile "pkgs/wezterm/ssh.lua";
         format = "binary";
       };
+      "id_ed25519.sshserver" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519.sshserver";
+        sopsFile = getEncryptFile "users/sumi/.ssh/id_ed25519.sshserver";
+        format = "binary";
+      };
+      "id_ed25519.homelab.dev" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519.homelab.dev";
+        sopsFile = getEncryptFile "users/sumi/.ssh/id_ed25519.homelab.dev";
+        format = "binary";
+      };
+      "id_ed25519.homelab.dev.pub" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519.homelab.dev.pub";
+        sopsFile = getEncryptFile "users/sumi/.ssh/id_ed25519.homelab.dev.pub";
+        format = "binary";
+      };
+      "id_ed25519.homelab" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519.homelab";
+        sopsFile = getEncryptFile "users/sumi/.ssh/id_ed25519.homelab";
+        format = "binary";
+      };
+      "id_ed25519.homelab.pub" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519.homelab.pub";
+        sopsFile = getEncryptFile "users/sumi/.ssh/id_ed25519.homelab.pub";
+        format = "binary";
+      };
       "aichat/config.yaml" = {
         path = "${config.xdg.configHome}/aichat/config.yaml";
         sopsFile = getEncryptFile "pkgs/ai-tools/config.yaml.txt";
