12be405803ddf5a23bd244153384129ce78cf2af
misumisumi <s.kobayashi@misumi-sumi.com>
2026-07-04 13:37:51 +0900

    fix(fzf): fix deprecated options

--- apps/user/core/fzf/default.nix
+++ apps/user/core/fzf/default.nix
@@ -8,16 +8,16 @@
       enableBashIntegration = true;
       enableZshIntegration = false; # Confilict "jeffreytse/zsh-vi-mode" so init my self
       # ALT+C option
-      changeDirWidgetCommand = "fd --type d";
-      changeDirWidgetOptions = [ "--preview 'tree -C {} | tree -200'" ];
+      changeDirWidget.command = "fd --type d";
+      changeDirWidget.options = [ "--preview 'tree -C {} | tree -200'" ];
       # CTRL+T option
-      fileWidgetCommand = "fd --type f";
-      fileWidgetOptions = [
+      fileWidget.command = "fd --type f";
+      fileWidget.options = [
         "--preview 'bat -n --color=always {}'"
         "--bind 'ctrl-/:change-preview-window(down|hidden|)'"
       ];
       # CTRL+R option
-      historyWidgetOptions = [
+      historyWidget.options = [
         "--preview 'echo {}' --preview-window up:3:hidden:wrap"
         "--bind 'ctrl-/:toggle-preview'"
         "--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
