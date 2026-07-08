27d4af5759d1e3f8a0b217c2f14b97812eb57145
misumisumi <s.kobayashi@misumi-sumi.com>
2026-07-08 11:47:52 +0900

    fix(gnome): add gnome-keyring's portal config to xdg-desktop-portal

--- apps/user/desktop/env/core/gnome/keyring/default.nix
+++ apps/user/desktop/env/core/gnome/keyring/default.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = [ pkgs.seahorse ];
+  xdg.portal.extraPortals = [ config.services.gnome-keyring.package ];
   services.gnome-keyring = {
     enable = true;
     components = [
