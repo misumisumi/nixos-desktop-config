c8bf10cf287e4948b2e40309d21abd08d10b5c5c
misumisumi <s.kobayashi@misumi-sumi.com>
2026-07-14 16:35:11 +0900

    fix(patches): add patches for spicetify-cli and deskreen until merge there

--- patches/default.nix
+++ patches/default.nix
@@ -55,6 +55,78 @@ final: prev: {
     proprietaryCodecs = true;
     enableWidevine = true;
   };
+  spicetify-cli = prev.spicetify-cli.overrideAttrs (old: {
+    ldflags = old.ldflags ++ [
+      "-X 'main.version=${old.version}'"
+    ];
+    nativeBuildInputs = old.nativeBuildInputs ++ [
+      prev.nodejs
+      prev.esbuild
+    ];
+
+    postBuild = ''
+      esbuild ./src/jsHelper/spicetifyWrapper/index.js \
+        --bundle --minify --target=chrome108 --format=iife \
+        --outfile=spicetifyWrapper.js
+    '';
+    postInstall = old.postInstall + ''
+      chmod -R u+w $out/share/spicetify/jsHelper
+      cp spicetifyWrapper.js $out/share/spicetify/jsHelper/spicetifyWrapper.js
+    '';
+  });
+  deskreen = prev.callPackage (
+    {
+      lib,
+      stdenvNoCC,
+      fetchurl,
+      appimageTools,
+    }:
+    appimageTools.wrapType2 rec {
+      pname = "deskreen";
+      version = "3.2.16";
+
+      src =
+        let
+          sources = {
+            x86_64-linux = {
+              arch = "x86_64";
+              hash = "sha256-JcVKRINEWHJXzpdyiMSzx+cp/BzHBhrXRxYizQmkerI=";
+            };
+          };
+        in
+        fetchurl {
+          url = "https://github.com/pavlobu/deskreen/releases/download/v${version}/deskreen-ce-${version}-${
+            sources.${stdenvNoCC.hostPlatform.system}.arch
+          }.AppImage";
+          inherit (sources.${stdenvNoCC.hostPlatform.system}) hash;
+        };
+      extraInstallCommands =
+        let
+          contents = appimageTools.extractType2 { inherit pname version src; };
+        in
+        ''
+          install -m 444 -D ${contents}/deskreen-ce.desktop $out/share/applications/deskreen-ce.desktop
+          install -m 444 -D ${contents}/usr/share/icons/hicolor/256x256/apps/deskreen-ce.png \
+            $out/share/icons/hicolor/512x512/apps/deskreen-ce.png
+          substituteInPlace $out/share/applications/deskreen-ce.desktop \
+            --replace-fail 'Exec=AppRun' 'Exec=deskreen'
+        '';
+
+      meta = {
+        description = "Turn any device into a secondary screen for your computer";
+        homepage = "https://deskreen.com";
+        license = lib.licenses.agpl3Only;
+        mainProgram = "deskreen";
+        maintainers = with lib.maintainers; [
+          leo248
+        ];
+        platforms = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+      };
+    }
+  ) { };
   python3 =
     let
       pythonPackagesOverlays = (prev.pythonPackagesOverlays or [ ]) ++ [
