c4cd5af608c2f04021f2299824fbe62698535db5
misumisumi <s.kobayashi@misumi-sumi.com>
2026-09-15 14:55:16 +0900

    fix(liveimg): add ssh pub key to known_hosts

--- machines/liveimg/system/user.nix
+++ machines/liveimg/system/user.nix
@@ -20,7 +20,15 @@
       "input"
     ];
     useDefaultShell = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCGcY4v0aRzAO+hLnGhEaU7JArt/Wrn8FuIgFcovlad sumi@mother-2021-03-12"
+    ];
     initialHashedPassword = lib.mkForce "$y$j9T$VM2hWCk7A3S5QKFhO1STx1$2Pq/o43bzXFMnpKtxiZiaFZnMuMym3EgqgysBk8sgA/";
   };
-  users.users.root.initialHashedPassword = lib.mkForce "$y$j9T$VM2hWCk7A3S5QKFhO1STx1$2Pq/o43bzXFMnpKtxiZiaFZnMuMym3EgqgysBk8sgA/";
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCGcY4v0aRzAO+hLnGhEaU7JArt/Wrn8FuIgFcovlad sumi@mother-2021-03-12"
+    ];
+    initialHashedPassword = lib.mkForce "$y$j9T$VM2hWCk7A3S5QKFhO1STx1$2Pq/o43bzXFMnpKtxiZiaFZnMuMym3EgqgysBk8sgA/";
+  };
 }
